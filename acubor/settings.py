dd76a3922b4e86c985fda72312498e4fabe48b68
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-14 23:44:18 +0545

    added settings_secret

--- acubor/settings.py
+++ acubor/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'acubor',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'acubor',
+        'PASSWORD': 'acubor',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -154,3 +154,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from settings_secret import *
+except ImportError:
+    pass
