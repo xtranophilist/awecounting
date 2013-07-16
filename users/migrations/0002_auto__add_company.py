28a1e3f47093c2ddfebe9eef3a580ddbb7c012e7
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-17 02:09:16 +0545

    created migration for adding company

---
+++ users/migrations/0002_auto__add_company.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Company'
+        db.create_table(u'users_company', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=254)),
+            ('location', self.gf('django.db.models.fields.TextField')()),
+            ('type_of_business', self.gf('django.db.models.fields.CharField')(max_length=254)),
+        ))
+        db.send_create_signal(u'users', ['Company'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Company'
+        db.delete_table(u'users_company')
+
+
+    models = {
+        u'users.company': {
+            'Meta': {'object_name': 'Company'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '254'}),
+            'type_of_business': ('django.db.models.fields.CharField', [], {'max_length': '254'})
+        },
+        u'users.user': {
+            'Meta': {'object_name': 'User'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '254', 'db_index': 'True'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '245'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['users']
