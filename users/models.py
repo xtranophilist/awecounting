e7f28acaf3147b88fc8a87454ecccba92fa6561f
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-16 23:18:26 +0545

    added User Manager

--- users/models.py
+++ users/models.py
@@ -1,6 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # from django.contrib.auth.models import PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=UserManager.normalize_email(email),
+            full_name=full_name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            full_name=full_name
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 # Create your models here.
@@ -11,8 +38,10 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
 
     # USERNAME_FIELD = 'username'
-    # USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']
 
     def __unicode__(self):
         return self.username
+
+    objects = UserManager()
