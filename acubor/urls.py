dbdd1084cb0db42636cb260939fdb3d0e888299a
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-20 19:15:32 +0545

    enabled admin panel

--- acubor/urls.py
+++ acubor/urls.py
@@ -2,21 +2,16 @@ from django.conf.urls import patterns, url, include
 from users import views
 # from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     # ('^$', TemplateView.as_view(template_name='site_index.html')),
     url(r'^$', views.index, name='index'),
     (r'^user/', include('users.urls')),
     (r'^user/', include('users.urls')),
     (r'^voucher/', include('voucher.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     )
