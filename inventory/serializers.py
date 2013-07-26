32265cf8ddc2cecd9911bf91f5db1e95caabb8e7
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-27 00:42:20 +0545

    added Inventory Item serializer

---
+++ inventory/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from models import Item
+from ledger.models import Account
+from tax.models import TaxScheme
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+
+
+class TaxSchemeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaxScheme
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    purchase_account = AccountSerializer
+    purchase_tax_scheme = TaxSchemeSerializer
+    sales_account = AccountSerializer
+    sales_tax_scheme = TaxSchemeSerializer
+    
+    class Meta:
+        model = Item
