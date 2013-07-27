971efb7a9525636c986b8bcf069a18ebc9a25639
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-27 23:07:35 +0545

    added company relation to tax scheme

---
+++ tax/migrations/0002_auto__add_field_taxscheme_company.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'TaxScheme.company'
+        db.add_column(u'tax_taxscheme', 'company',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['users.Company']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'TaxScheme.company'
+        db.delete_column(u'tax_taxscheme', 'company_id')
+
+
+    models = {
+        u'tax.taxscheme': {
+            'Meta': {'object_name': 'TaxScheme'},
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Company']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'percent': ('django.db.models.fields.FloatField', [], {})
+        },
+        u'users.company': {
+            'Meta': {'object_name': 'Company', 'db_table': "u'company'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '254'}),
+            'type_of_business': ('django.db.models.fields.CharField', [], {'max_length': '254'})
+        }
+    }
+
+    complete_apps = ['tax']
--- tax/models.py
+++ tax/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from users.models import Company
 
 
 class TaxScheme(models.Model):
     name = models.CharField(max_length=100)
     percent = models.FloatField()
+    company = models.ForeignKey(Company)
 
     def __unicode__(self):
         return self.name
-
