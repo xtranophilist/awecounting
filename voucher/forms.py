5cd55cb9c4c047c3f5811b9b9ed943ffc7c173fc
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-27 23:41:30 +0545

    added suffix and prefix for invoice_no

--- voucher/forms.py
+++ voucher/forms.py
@@ -5,7 +5,10 @@ from models import SalesVoucher, Currency
 
 class SalesVoucherForm(KOModelForm):
     party = forms.CharField(widget=forms.TextInput(), label='To')
-    currency = forms.ModelChoiceField(Currency.objects.all(), empty_label = None)
+    currency = forms.ModelChoiceField(Currency.objects.all(), empty_label=None)
+    invoice_no = forms.CharField(
+               widget=forms.TextInput(attrs={'maxlength': 5, 'size': 5}))
+
 
     class Meta:
         model = SalesVoucher
