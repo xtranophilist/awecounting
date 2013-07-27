9d8a2db7cfedcb0102764b0763ea9ff9833c2956
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-28 02:54:40 +0545

    invoice no. prefix and suffix are retrieved from Company Settings

--- voucher/views.py
+++ voucher/views.py
@@ -2,14 +2,16 @@ from forms import SalesVoucherForm
 from models import SalesVoucher
 from django.shortcuts import render
 from voucher.serializers import SalesVoucherSerializer
+from core.models import CompanySetting
 
 
-def sales(request):
+def invoice(request):
+    company_setting = CompanySetting.objects.get(company=request.user.company)
     sales_voucher = SalesVoucher()
     form = SalesVoucherForm(data=request.POST, instance=sales_voucher)
     voucher_data = SalesVoucherSerializer(sales_voucher).data
     voucher_data['read_only'] = {
-        'invoice_prefix': 'INNV-',
-        'invoice_suffix': '...',
+        'invoice_prefix': company_setting.invoice_prefix,
+        'invoice_suffix': company_setting.invoice_suffix,
     }
     return render(request, 'invoice.html', {'form': form, 'data': voucher_data})
