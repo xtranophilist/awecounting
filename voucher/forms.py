b5996dfdd9305620c12916d80e7a1a7717011293
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-27 22:12:29 +0545

    [invoice] removed default blank from currency dropdown

--- voucher/forms.py
+++ voucher/forms.py
@@ -1,10 +1,11 @@
 from acubor.lib import KOModelForm
 from django import forms
-from models import SalesVoucher
+from models import SalesVoucher, Currency
 
 
 class SalesVoucherForm(KOModelForm):
     party = forms.CharField(widget=forms.TextInput(), label='To')
+    currency = forms.ModelChoiceField(Currency.objects.all(), empty_label = None)
 
     class Meta:
         model = SalesVoucher
