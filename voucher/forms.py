bc5f3c1db5b3e046de2bd2b97966fd9950129f9c
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-28 00:43:47 +0545

    added invoice suffix/prefix binding to view

--- voucher/forms.py
+++ voucher/forms.py
@@ -6,9 +6,6 @@ from models import SalesVoucher, Currency
 class SalesVoucherForm(KOModelForm):
     party = forms.CharField(widget=forms.TextInput(), label='To')
     currency = forms.ModelChoiceField(Currency.objects.all(), empty_label=None)
-    invoice_no = forms.CharField(
-               widget=forms.TextInput(attrs={'maxlength': 5, 'size': 5}))
-
 
     class Meta:
         model = SalesVoucher
