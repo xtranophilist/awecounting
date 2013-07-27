512ad43bdece19d3687801ba4773e62d1f1d92ff
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-28 04:05:55 +0545

    moved custom ko bindings, extensions to ko_lib

---
+++ js/ko_lib.js
@@ -0,0 +1,89 @@
+//Custom Bindings
+
+ko.bindingHandlers.typeahead = {
+            init: function (element, valueAccessor) {
+                $(element).attr("autocomplete", "off")
+                .typeahead({
+                    'source': ko.utils.unwrapObservable(valueAccessor())
+                });
+            }
+        };
+
+ko.bindingHandlers.editableText = {
+    init: function(element, valueAccessor) {
+        $(element).on('blur', function() {
+            var observable = valueAccessor();
+            observable( $(this).text() );
+        });
+    },
+    update: function(element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        $(element).text(value);
+    }
+};
+
+ko.bindingHandlers.numeric = {
+    init: function(element, valueAccessor) {
+        $(element).on('keydown', function(event) {
+
+            // Allow: backspace, delete, tab, escape, and enter
+            if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+                // Allow: Ctrl combinations
+                (event.ctrlKey === true) ||
+                //Allow decimal symbol (.)
+                (event.keyCode === 190) ||
+                // Allow: home, end, left, right
+                (event.keyCode >= 35 && event.keyCode <= 39)) {
+                // let it happen, don't do anything
+                return;
+            }
+            else {
+                // Ensure that it is a number and stop the keypress
+                if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                    event.preventDefault();
+                }
+            }
+        });
+    },
+    update: function(element, valueAccessor) {
+    }
+};
+
+//Custom Observable Extensions
+ko.extenders.numeric = function(target, precision) {
+    //create a writeable computed observable to intercept writes to our observable
+    var result = ko.computed({
+        read: target,  //always return the original observables value
+        write: function(newValue) {
+            var current = target(),
+                roundingMultiplier = Math.pow(10, precision),
+                newValueAsNum = isNaN(newValue) ? current : parseFloat(+newValue),
+                valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
+
+            //only write if it changed
+            if (valueToWrite !== current) {
+                target(valueToWrite);
+            } else {
+                //if the rounded value is the same, but a different value was written, force a notification for the current field
+                if (newValue !== current) {
+                    target.notifySubscribers(valueToWrite);
+                }
+            }
+        }
+    });
+
+    //initialize with current value to make sure it is rounded appropriately
+    result(target());
+
+    //return the new computed observable
+    return result;
+};
+
+//Other useful KO-related functions
+function setBinding(id, value) {
+    var el = document.getElementById(id);
+    if (el) {
+        el.setAttribute('data-bind', value);
+    }
+}
+
