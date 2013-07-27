4bfab319876e2eb696112810ea31e8871595162a
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-28 03:30:11 +0545

    created migrations for changed db design

---
+++ voucher/migrations/0005_auto__add_field_invoice_company__add_field_purchasevoucher_company.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Invoice.company'
+        db.add_column('invoice', 'company',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=5, to=orm['users.Company']),
+                      keep_default=False)
+
+        # Adding field 'PurchaseVoucher.company'
+        db.add_column(u'voucher_purchasevoucher', 'company',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=5, to=orm['users.Company']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Invoice.company'
+        db.delete_column('invoice', 'company_id')
+
+        # Deleting field 'PurchaseVoucher.company'
+        db.delete_column(u'voucher_purchasevoucher', 'company_id')
+
+
+    models = {
+        u'core.currency': {
+            'Meta': {'object_name': 'Currency', 'db_table': "'currency'"},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latest_usd_rate': ('django.db.models.fields.FloatField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'core.party': {
+            'Meta': {'object_name': 'Party', 'db_table': "'party'"},
+            'address': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Company']"}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '254'}),
+            'fax': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '254'}),
+            'phone_no': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'inventory.item': {
+            'Meta': {'object_name': 'Item'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Company']"}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '254'}),
+            'purchase_account': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'purchase_items'", 'to': u"orm['ledger.Account']"}),
+            'purchase_price': ('django.db.models.fields.FloatField', [], {}),
+            'purchase_tax_scheme': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'purchase_items'", 'to': u"orm['tax.TaxScheme']"}),
+            'sales_account': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sales_items'", 'to': u"orm['ledger.Account']"}),
+            'sales_price': ('django.db.models.fields.FloatField', [], {}),
+            'sales_tax_scheme': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sales_items'", 'to': u"orm['tax.TaxScheme']"})
+        },
+        u'ledger.account': {
+            'Meta': {'object_name': 'Account'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Company']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'tax.taxscheme': {
+            'Meta': {'object_name': 'TaxScheme'},
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Company']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'percent': ('django.db.models.fields.FloatField', [], {})
+        },
+        u'users.company': {
+            'Meta': {'object_name': 'Company', 'db_table': "u'company'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '254'}),
+            'type_of_business': ('django.db.models.fields.CharField', [], {'max_length': '254'})
+        },
+        u'voucher.invoice': {
+            'Meta': {'object_name': 'Invoice', 'db_table': "'invoice'"},
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Company']"}),
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Currency']"}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'due_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice_no': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Party']"}),
+            'reference': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'tax': ('django.db.models.fields.CharField', [], {'default': "'inclusive'", 'max_length': '10'})
+        },
+        u'voucher.particular': {
+            'Meta': {'object_name': 'Particular'},
+            'account': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ledger.Account']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'discount': ('django.db.models.fields.FloatField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'particulars'", 'to': u"orm['voucher.Invoice']"}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['inventory.Item']"}),
+            'quantity': ('django.db.models.fields.FloatField', [], {'default': '1'}),
+            'sn': ('django.db.models.fields.IntegerField', [], {}),
+            'tax_scheme': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tax.TaxScheme']"}),
+            'unit_price': ('django.db.models.fields.FloatField', [], {})
+        },
+        u'voucher.purchasevoucher': {
+            'Meta': {'object_name': 'PurchaseVoucher'},
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Company']"}),
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Currency']"}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'due_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice_no': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Party']"}),
+            'reference': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'tax': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        }
+    }
+
+    complete_apps = ['voucher']
