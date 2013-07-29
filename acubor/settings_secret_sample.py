c20a765433c1c8fea55b77a1548e7cf92051c7be
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-30 00:20:53 +0545

    updated settings-secret sample file

--- acubor/settings_secret_sample.py
+++ acubor/settings_secret_sample.py
@@ -1,8 +1,7 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'acubor',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'acubor',
         'USER': 'acubor',
         'PASSWORD': 'acubor',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
