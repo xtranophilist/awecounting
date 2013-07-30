ac32c0523043cd9695e4698d548adff7599bf3ab
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-30 21:41:33 +0545

    added date-picker binding to .due-date

--- voucher/forms.py
+++ voucher/forms.py
@@ -6,8 +6,8 @@ from models import Invoice, Currency
 class InvoiceForm(KOModelForm):
     party = forms.CharField(widget=forms.TextInput(), label='To')
     currency = forms.ModelChoiceField(Currency.objects.all(), empty_label=None)
-    date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class':'date-picker'}))
-    due_date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class':'date-picker'}))
+    date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'date-picker'}))
+    due_date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'due-date'}))
 
     class Meta:
         model = Invoice
