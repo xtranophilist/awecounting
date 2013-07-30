3a86bbc2e9793bcdfbcb66e25df67854815411a8
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-30 23:19:12 +0545

    created view and template for creating new item

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,9 +5,9 @@ urlpatterns = patterns('',
     # url(r'^create/$', views.CreateAccount.as_view(template_name='create_form.html'), name='create_account'),
     # url(r'^$', views.index, name='inventory_items'),
     # url(r'^(?P<pk>[0-9]+)/$', views.DetailAccount.as_view(template_name='detail.html'), name='detail_view'),
-    url(r'^create/$', views.CreateItem.as_view(template_name='create_form.html'), name='create_item'),
-    url(r'^$', views.ListItem.as_view(template_name='list.html'), name='list_items'),
+    url(r'^create/$', views.item_form, name='create_item'),
+    # url(r'^$', views.ListItem.as_view(template_name='list.html'), name='list_items'),
     url(r'^items/json/$', views.items_as_json, name='items_as_json'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailItem.as_view(template_name='detail.html'), name='detail_item'),
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailItem.as_view(template_name='detail.html'), name='detail_item'),
     )
 
--- inventory/views.py
+++ inventory/views.py
@@ -1,32 +1,39 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 import json
 
-from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic.detail import DetailView
-from django.views.generic import ListView
-from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
 
 from models import Item
 
 from serializers import ItemSerializer
+from forms import ItemForm
 
 
-class DetailItem(DetailView):
-    model = Item
+def item_form(request):
+    item = Item()
+    form = ItemForm(data=request.POST, instance=item)
+    if request.POST:
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.company = request.user.company
+            item.save()
+    return render(request, 'item_form.html', {'form': form})
 
-
-class CreateItem(CreateView):
-    model = Item
-    success_url = reverse_lazy('list_items')
-
-
-class ListItem(ListView):
-    model = Item
-
-
-class UpdateItem(UpdateView):
-    model = Item
+# class DetailItem(DetailView):
+#     model = Item
+#
+#
+# class CreateItem(CreateView):
+#     model = Item
+#     success_url = reverse_lazy('list_items')
+#
+#
+# class ListItem(ListView):
+#     model = Item
+#
+#
+# class UpdateItem(UpdateView):
+#     model = Item
 
 
 def items_as_json(request):
