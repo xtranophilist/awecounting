b9a473f1c2de05e8a83b1455402a6f9094ac02ef
Dipesh Acharya <xtranophilist@gmail.com>
2013-07-30 11:29:18 +0545

    added support for HTML5 input type fields; added date and due date

--- acubor/lib.py
+++ acubor/lib.py
@@ -1,6 +1,27 @@
 from django.forms import ModelForm
+from django.forms.widgets import TextInput, DateInput, DateTimeInput, TimeInput
+
 
 class KOModelForm(ModelForm):
+
+    class EmailTypeInput(TextInput):
+        input_type = 'email'
+
+    class NumberTypeInput(TextInput):
+        input_type = 'number'
+
+    class TelephoneTypeInput(TextInput):
+        input_type = 'tel'
+
+    class DateTypeInput(DateInput):
+        input_type = 'date'
+
+    class DateTimeTypeInput(DateTimeInput):
+        input_type = 'datetime'
+
+    class TimeTypeInput(TimeInput):
+        input_type = 'time'
+
     def __init__(self, *args, **kwargs):
         super(KOModelForm, self).__init__(*args, **kwargs)
         self.refine()
