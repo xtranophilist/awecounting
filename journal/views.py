a0d0908c3f5dfee7cc3707edc5614d8dd0827b73
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-02 16:02:07 +0545

    js/css path update

--- journal/views.py
+++ journal/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from journal.models import DayJournal, DayCashPayment
+from datetime import date
 
 
 def day_journal(request, id=None):
-    day_journal = DayJournal()
+    day_journal = DayJournal(date=date.today(), company=request.user.company)
     base_template = 'dashboard.html'
 
     return render(request, 'day_journal.html', {
