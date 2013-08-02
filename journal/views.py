165310bf8f43e67958b05cf1f921615e538bf704
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-03 01:11:47 +0545

    created table and rows binding for day cash sales

--- journal/views.py
+++ journal/views.py
@@ -8,7 +8,6 @@ def day_journal(request, id=None):
     day_journal = DayJournal(date=date.today(), company=request.user.company)
     day_journal_data = DayJournalSerializer(day_journal).data
     base_template = 'dashboard.html'
-
     return render(request, 'day_journal.html', {
         'journal_data': day_journal_data,
         'base_template': base_template,
