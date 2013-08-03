2706aadb791470be996312f3c7ef975055ba3958
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-04 02:55:59 +0545

    added submodel saving

--- journal/urls.py
+++ journal/urls.py
@@ -4,5 +4,6 @@ import views
 urlpatterns = patterns('',
                        url(r'^day/$', views.day_journal, name='new_day_journal'),
                        url(r'^day/(?P<id>[0-9]+)/$', views.day_journal, name='view_day_journal'),
+                       url(r'^day/save/(?P<submodel>[a-zA-Z0-9_.-]+)/$', views.save_submodel, name='save_submodel'),
                        )
 
--- journal/views.py
+++ journal/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from journal.models import DayJournal, DayCashPayment
 from datetime import date
 from journal.serializers import DayJournalSerializer
+from django.http import HttpResponse
+import json
 
 
 def day_journal(request, id=None):
@@ -12,3 +14,29 @@ def day_journal(request, id=None):
         'journal': day_journal_data,
         'base_template': base_template,
     })
+
+
+def save_journal_if_not_exist(date, company):
+    journal, created = DayJournal.objects.get_or_create(date=date, company=company)
+    if created:
+        journal.save()
+    return journal
+
+
+def save_submodel(request, submodel):
+    params = json.loads(request.body)
+    required = params.get('required')
+    for row in params.get('rows'):
+        valid = True
+        for attr in required:
+            # if one of the required attributes isn't received
+            if not attr in row:
+                valid = False
+            # if one of the required attributes is an empty string
+            if row.get(attr) == "":
+                valid = False
+        if not valid:
+            continue
+        print row
+    journal = save_journal_if_not_exist(params['journal_date'], request.user.company)
+    return HttpResponse(json.dumps({}), mimetype="application/json")
