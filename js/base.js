d5bb3c55fbace6d9798437ad29dd0ca7d8db0d1d
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-03 23:34:50 +0545

    moved TableViewModel to base.js

--- js/base.js
+++ js/base.js
@@ -31,3 +31,66 @@ $.ajaxSetup({
          }
      }
 });
+
+function TableViewModel(data, row_model, save_to_url){
+    console.log(data);
+    var self = this;
+    for (var k in data)
+        self[k]=data[k];
+
+    self.rows = ko.observableArray(ko.utils.arrayMap(data.rows, function(item) {
+        return new row_model(item);
+    }));
+
+    self.hasNoRows = ko.computed(function(){
+        return self.rows().length === 0;
+    });
+
+    self.addRow = function() {
+        var new_item_index = self.rows().length+1;
+        self.rows.push(new row_model());
+    };
+
+    self.removeRow = function(row) {
+        self.rows.remove(row);
+    };
+
+    if (self.hasNoRows){
+        self.addRow();
+    }
+
+    self._initial_rows = $.extend(true, {}, self.rows());
+
+    if (typeof(save_to_url) != 'undefined'){
+        self.save = function(model, e){
+            var el = get_target(e);
+            el.on('mouseover', function() {
+                el.html('Save');
+            });
+            el.html('Saving');
+            $.ajax({
+                type: "POST",
+                url: save_to_url,
+                data: ko.toJSON(self),
+                success: function(msg){
+                    el.html('Save');
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    el.html("Saving Failed!");
+                }
+            });
+
+        }
+    }
+    else{
+        self.save= function(){
+            throw new Error("'save_to_url' not passed to TableViewModel or save() not implemented!");
+        }
+    }
+
+    self.reset= function(){
+        console.log(self.rows());
+        console.log(self._initial_rows[0]);
+        self.rows(self._initial_rows);
+    }
+}
--- js/day_journal.js
+++ js/day_journal.js
@@ -10,76 +10,12 @@ function DayJournal(data){
             self.items = data;
         }
     });
+    data.day_cash_sales.journal_date = self.date;
     self.day_cash_sales = new TableViewModel(data.day_cash_sales, DayCashSalesRow, '/journal/day_cash_sales/save');
 }
 
 
-function TableViewModel(data, row_model, save_to_url){
-    var self = this;
-    for (var k in data)
-        self[k]=data[k];
 
-    self.rows = ko.observableArray(ko.utils.arrayMap(data.rows, function(item) {
-        return new row_model(item);
-    }));
-
-
-
-    self.hasNoRows = ko.computed(function(){
-        return self.rows().length === 0;
-    });
-
-    self.addRow = function() {
-        var new_item_index = self.rows().length+1;
-        self.rows.push(new row_model());
-    };
-
-    self.removeRow = function(row) {
-        self.rows.remove(row);
-    };
-
-    if (self.hasNoRows){
-        self.addRow();
-    }
-
-    self._initial_rows = $.extend(true, {}, self.rows());
-
-    if (typeof(save_to_url) != 'undefined'){
-        self.save = function(model, e){
-            var el = get_target(e);
-            el.on('mouseover', function() {
-                el.html('Save')
-            });
-            el.html('Saving');
-            $.ajax({
-                type: "POST",
-                url: save_to_url,
-                data: ko.toJSON(self),
-                success: function(msg){
-                    el.html('Save');
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    el.html("Saving Failed!");
-                }
-            });
-
-        }
-    }
-    else{
-        self.save= function(){
-            throw new Error("'save_to_url' not passed to TableViewModel or save() not implemented!");
-        }
-    }
-
-
-    self.reset= function(){
-        console.log(self.rows());
-        console.log(self._initial_rows[0]);
-        self.rows(self._initial_rows);
-    }
-
-
-}
 
 function DayCashSalesRow(row){
     var self = this;
