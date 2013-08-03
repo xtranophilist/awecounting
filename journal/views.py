170f85b1f9017751b6b127a64e1dfe18399be49a
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-04 04:27:42 +0545

    fixed returning of index:id dict for row item

--- journal/views.py
+++ journal/views.py
@@ -16,8 +16,9 @@ def day_journal(request, id=None):
     })
 
 
-def save_journal_if_not_exist(date, company):
-    journal, created = DayJournal.objects.get_or_create(date=date, company=company)
+def get_journal(request):
+    journal, created = DayJournal.objects.get_or_create(date=json.loads(request.body).get('journal_date'), \
+                                                        company=request.user.company)
     if created:
         journal.save()
     return journal
@@ -26,7 +27,7 @@ def save_journal_if_not_exist(date, company):
 def save_submodel(request, submodel):
     params = json.loads(request.body)
     required = params.get('required')
-    day_journal = save_journal_if_not_exist(params.get('journal_date'), request.user.company)
+    day_journal = get_journal(request)
     dct = {}
     for index, row in enumerate(params.get('rows')):
         valid = True
@@ -36,7 +37,6 @@ def save_submodel(request, submodel):
                 valid = False
         if not valid:
             continue
-        print row
         if not 'id' in row:
             day_cash_sales = DayCashSales(sn=index+1, item_id=row.get('item_id'), amount=row.get('amount'),\
                                           quantity=row.get('quantity'), day_journal=day_journal)
@@ -48,5 +48,5 @@ def save_submodel(request, submodel):
         day_cash_sales.amount = row.get('amount')
         day_cash_sales.quantity = row.get('quantity')
         day_cash_sales.save()
-        dct[index] = day_cash_sales
+        dct[index] = day_cash_sales.id
     return HttpResponse(json.dumps(dct), mimetype="application/json")
