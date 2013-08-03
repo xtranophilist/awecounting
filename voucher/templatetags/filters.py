ea09d3ce2c385bb2c84d712a1118d51e934896e6
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-03 16:51:52 +0545

    fixed jsonify filter such that datetime is also serialized

--- voucher/templatetags/filters.py
+++ voucher/templatetags/filters.py
@@ -1,12 +1,21 @@
 from django.core import serializers
 from django.db.models.query import QuerySet
-from django.utils import simplejson
 from django.template import Library
 from django.utils.safestring import mark_safe
 from django.db.models import Model
 # from django.forms.models import model_to_dict
+import json
 
 register = Library()
+
+
+def handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    # elif isinstance(obj, ...):
+    #     return ...
+    else:
+        raise TypeError, 'Object of type %s with value of %s is not JSON serializable' % (type(obj), repr(obj))
 
 
 @register.filter
@@ -16,8 +25,8 @@ def jsonify(object):
     if isinstance(object, Model):
         model_dict = object.__dict__
         del model_dict['_state']
-        return mark_safe(simplejson.dumps(model_dict))
-    return mark_safe(simplejson.dumps(object))
+        return mark_safe(json.dumps(model_dict))
+    return mark_safe(json.dumps(object, default=handler))
 
 
 @register.filter
@@ -27,7 +36,7 @@ def user_to_json(user):
                 user._setup()
             user = user._wrapped
     user_dict = {'id': user.id, 'username': user.username}
-    return mark_safe(simplejson.dumps(user_dict))
+    return mark_safe(json.dumps(user_dict))
 
 
 @register.filter
