224066bb1a3962cc943766a3216475f56f4a8fbb
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-06 21:44:36 +0545

    added select2 custom binding

--- js/ko_lib.js
+++ js/ko_lib.js
@@ -1,4 +1,28 @@
+
 //Custom Bindings
+
+ko.bindingHandlers.select2 = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+        var obj = valueAccessor(),
+            allBindings = allBindingsAccessor(),
+            lookupKey = allBindings.lookupKey;
+        $(element).select2(obj);
+        if (lookupKey) {
+            var value = ko.utils.unwrapObservable(allBindings.value);
+            $(element).select2('data', ko.utils.arrayFirst(obj.data.results, function(item) {
+                return item[lookupKey] === value;
+            }));
+        }
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            $(element).select2('destroy');
+        });
+    },
+    update: function(element) {
+        $(element).trigger('change');
+    }
+};
+
 ko.bindingHandlers.typeahead = {
   init: function (element, valueAccessor) {
       var el = $(element);
