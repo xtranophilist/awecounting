c4e1f37a4ce7851fbc9bda73c64ea600b92a4ec9
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-10 02:00:05 +0545

    fixed datepicker related form stuffs

--- voucher/forms.py
+++ voucher/forms.py
@@ -8,7 +8,6 @@ class InvoiceForm(KOModelForm):
     party = forms.ModelChoiceField(Party.objects.all(), empty_label=None, widget=forms.Select(attrs={'class': 'select2'}))
     currency = forms.ModelChoiceField(Currency.objects.all(), empty_label=None,
                                       widget=forms.Select(attrs={'class': 'select2'}))
-    date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'date-picker'}))
 
     class Meta:
         model = Invoice
@@ -18,8 +17,6 @@ class InvoiceForm(KOModelForm):
 class PurchaseVoucherForm(KOModelForm):
     # party = forms.CharField(widget=forms.TextInput())
     currency = forms.ModelChoiceField(Currency.objects.all(), empty_label=None)
-    date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'date-picker'}))
-    due_date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'due-date'}))
     attachment = forms.FileField(
         label='Add an attachment',
         help_text='',
