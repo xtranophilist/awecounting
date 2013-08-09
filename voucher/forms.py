6a8fa5ad8806b878eeca87e1458632af4ed5869c
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-09 11:59:25 +0545

    implemented select2 for invoice party selection

--- voucher/forms.py
+++ voucher/forms.py
@@ -1,9 +1,11 @@
 from acubor.lib import KOModelForm
 from django import forms
-from core.models import Currency
+from core.models import Currency, Party
 from voucher.models import Invoice, PurchaseVoucher
 
+
 class InvoiceForm(KOModelForm):
+    party = forms.ModelChoiceField(Party.objects.all(), empty_label=None, widget=forms.Select(attrs={'class': 'select2'}))
     # party = forms.CharField(widget=forms.TextInput(), label='To')
     currency = forms.ModelChoiceField(Currency.objects.all(), empty_label=None)
     date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'date-picker'}))
@@ -14,7 +16,7 @@ class InvoiceForm(KOModelForm):
 
 
 class PurchaseVoucherForm(KOModelForm):
-    # party = forms.CharField(widget=forms.TextInput(), label='To')
+    # party = forms.CharField(widget=forms.TextInput())
     currency = forms.ModelChoiceField(Currency.objects.all(), empty_label=None)
     date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'date-picker'}))
     due_date = forms.DateField(widget=KOModelForm.DateTypeInput(attrs={'class': 'due-date'}))
