18cce4bf7a8250cd5eff284d7d3e04ba871d6aec
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-11 04:19:55 +0545

    fixed saving of DayCashPurchase

--- journal/views.py
+++ journal/views.py
@@ -43,33 +43,20 @@ def save_day_cash_sales(request):
 
 
 def save_day_cash_purchase(request):
-    pass
-
-
-# def save_day_cash_purchase(request):
-#     params = json.loads(request.body)
-#     required = ['item_id', 'amount']
-#     day_journal = get_journal(request)
-#     dct = {}
-#     DayCashPurchase.objects.filter(day_journal=day_journal).delete()
-#     for index, row in enumerate(params.get('rows')):
-#         valid = True
-#         for attr in required:
-#             # if one of the required attributes isn't received or is an empty string
-#             if not attr in row or row.get(attr) == "":
-#                 valid = False
-#         if not valid:
-#             continue
-#         day_cash_purchase = DayCashPurchase(sn=index + 1, item_id=row.get('item_id'), amount=row.get('amount'),
-#                                             quantity=row.get('quantity'), day_journal=day_journal, id=row.get('id'))
-#         day_cash_purchase.sn = index + 1
-#         day_cash_purchase.item_id = row.get('item_id')
-#         day_cash_purchase.amount = row.get('amount')
-#         if row.get('quantity'):
-#             day_cash_purchase.quantity = row.get('quantity')
-#         day_cash_purchase.save()
-#         dct[index] = day_cash_purchase.id
-#     return HttpResponse(json.dumps(dct), mimetype="application/json")
+    params = json.loads(request.body)
+    dct = {}
+    model = DayCashPurchase
+    for index, row in enumerate(params.get('rows')):
+        if invalid(row, ['item_id', 'amount', 'quantity']):
+            continue
+        values = {'sn': index+1, 'item_id': row.get('item_id'), 'amount': row.get('amount'),
+                  'quantity': row.get('quantity'), 'day_journal': get_journal(request)}
+        submodel, created = model.objects.get_or_create(id=row.get('id'), defaults=values)
+        if not created:
+            submodel = save_model(submodel, values)
+        dct[index] = submodel.id
+    delete_rows(params.get('deleted_rows'), model)
+    return HttpResponse(json.dumps(dct), mimetype="application/json")
 
 
 def save_day_cash_receipt(request):
