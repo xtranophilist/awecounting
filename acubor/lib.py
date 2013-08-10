9ed92db6ef26df2f2cc40644f1f2c56d0b19c96a
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-11 00:44:06 +0545

    added File Upload by extension

--- acubor/lib.py
+++ acubor/lib.py
@@ -1,25 +1,48 @@
-from django.forms import ModelForm
-from django.forms.widgets import TextInput, DateInput, DateTimeInput, TimeInput
+import os
+from django import forms
 
 
-class KOModelForm(ModelForm):
+class ExtFileField(forms.FileField):
+    """
+    Same as forms.FileField, but you can specify a file extension whitelist.
 
-    class EmailTypeInput(TextInput):
+    Traceback (most recent call last):
+    ...
+    ValidationError: [u'Not allowed filetype!']
+    """
+    def __init__(self, *args, **kwargs):
+        ext_whitelist = kwargs.pop("ext_whitelist")
+        self.ext_whitelist = [i.lower() for i in ext_whitelist]
+
+        super(ExtFileField, self).__init__(*args, **kwargs)
+
+    def clean(self, *args, **kwargs):
+        data = super(ExtFileField, self).clean(*args, **kwargs)
+        filename = data.name
+        ext = os.path.splitext(filename)[1]
+        ext = ext.lower()
+        if ext not in self.ext_whitelist:
+            raise forms.ValidationError("File type not allowed!")
+
+
+class KOModelForm(forms.ModelForm):
+
+    class EmailTypeInput(forms.widgets.TextInput):
         input_type = 'email'
 
-    class NumberTypeInput(TextInput):
+    class NumberTypeInput(forms.widgets.TextInput):
         input_type = 'number'
 
-    class TelephoneTypeInput(TextInput):
+    class TelephoneTypeInput(forms.widgets.TextInput):
         input_type = 'tel'
 
-    class DateTypeInput(DateInput):
+    class DateTypeInput(forms.widgets.DateInput):
         input_type = 'date'
 
-    class DateTimeTypeInput(DateTimeInput):
+    class DateTimeTypeInput(forms.widgets.DateTimeInput):
         input_type = 'datetime'
 
-    class TimeTypeInput(TimeInput):
+    class TimeTypeInput(forms.widgets.TimeInput):
         input_type = 'time'
 
     def __init__(self, *args, **kwargs):
