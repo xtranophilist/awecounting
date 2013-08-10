b88b9016fe634ac8f12ac450ab95e01e3eb2a270
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-11 03:37:56 +0545

    [invoadded support for row specific errors

--- voucher/views.py
+++ voucher/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from datetime import date
 
 from forms import InvoiceForm, PurchaseVoucherForm
-from voucher.models import Invoice, PurchaseVoucher, Particular
+from voucher.models import Invoice, PurchaseVoucher, InvoiceParticular, PurchaseParticular
 from voucher.serializers import InvoiceSerializer, PurchaseVoucherSerializer
 from django.http import HttpResponse
 import json
@@ -46,14 +46,13 @@ def invoice(request, id=None):
 
 def save_invoice(request):
     params = json.loads(request.body)
-    dct = {}
+    dct = {'rows': {}}
     invoice_values = {'party_id': params.get('party'), 'invoice_no': params.get('invoice_no'),
                       'reference': params.get('reference'), 'date': params.get('date'),
                       'due_date': params.get('due_date'), 'tax': params.get('tax'),
                       'currency_id': params.get('currency'), 'company': request.user.company}
     try:
         if params.get('id'):
-            # invoice, created = Invoice.objects.get_or_create(id=params.get('id'), defaults=invoice_values)
             invoice = Invoice.objects.get(id=params.get('id'))
         else:
             invoice = Invoice()
@@ -65,7 +64,7 @@ def save_invoice(request):
             dct['error_message'] = '; '.join(e.messages)
         else:
             dct['error_message'] = 'Error in form data!'
-    model = Particular
+    model = InvoiceParticular
     for index, row in enumerate(params.get('particulars').get('rows')):
         if invalid(row, ['item_id', 'unit_price', 'quantity']):
             continue
@@ -75,7 +74,7 @@ def save_invoice(request):
         submodel, created = model.objects.get_or_create(id=row.get('id'), defaults=values)
         if not created:
             submodel = save_model(submodel, values)
-        dct[index] = submodel.id
+        dct['rows'][index] = submodel.id
     delete_rows(params.get('particulars').get('deleted_rows'), model)
     return HttpResponse(json.dumps(dct), mimetype="application/json")
 
@@ -88,6 +87,9 @@ def purchase_voucher(request, id=None):
         return redirect('/settings/company')
     voucher = PurchaseVoucher(date=date.today(), currency=company_setting.default_currency)
     if request.POST:
+        particulars = json.loads(request.POST['particulars'])
+        import pdb
+        pdb.set_trace()
         form = PurchaseVoucherForm(request.POST, request.FILES)
         if form.is_valid():
             voucher = form.save(commit=False)
