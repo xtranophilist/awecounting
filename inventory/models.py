558fbcc029ffcb8951fd2238ed9cd68d56a0c374
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-12 03:51:00 +0545

    added inventory item category

--- inventory/models.py
+++ inventory/models.py
@@ -4,10 +4,19 @@ from ledger.models import Account
 from users.models import Company
 
 
+class Category(models.Model):
+    code = models.CharField(max_length=10, blank=True, null=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return '[' + self.code + '] ' + self.name
+
+
 class Item(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=254)
-    description = models.TextField(null=True)
+    description = models.TextField(blank=True, null=True)
     purchase_price = models.FloatField()
     purchase_account = models.ForeignKey(Account, related_name='purchase_items')
     purchase_tax_scheme = models.ForeignKey(TaxScheme, verbose_name=u'Tax Rate', related_name='purchase_items')
@@ -15,6 +24,7 @@ class Item(models.Model):
     sales_account = models.ForeignKey(Account, related_name='sales_items')
     sales_tax_scheme = models.ForeignKey(TaxScheme, verbose_name=u'Tax Rate', related_name='sales_items')
     company = models.ForeignKey(Company)
+    category = models.ForeignKey(Category)
 
     def __unicode__(self):
         return '[' + self.code + '] ' + self.name
