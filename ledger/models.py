d4b0cf565578f0a0beb8c950799a47860ceb5e13
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-12 15:10:52 +0545

    added inventory account

--- ledger/models.py
+++ ledger/models.py
@@ -12,3 +12,15 @@ class Account(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class InventoryAccount(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company)
+
+    def get_absolute_url(self):
+        return '/inventory_account/' + str(self.id)
+
+    def __unicode__(self):
+        return self.name
