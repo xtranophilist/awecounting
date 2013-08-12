9854aa917e0feaa22f9d6c3c859327225b6dc316
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-12 15:29:37 +0545

    further manual day_journal to day_book refactoring

--- daybook/urls.py
+++ daybook/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-                       url(r'^day/$', views.day_book, name='new_day_journal'),
-                       url(r'^day/(?P<id>[0-9]+)/$', views.day_book, name='view_day_journal'),
+                       url(r'^day/$', views.day_book, name='new_day_book'),
+                       url(r'^day/(?P<id>[0-9]+)/$', views.day_book, name='view_day_book'),
                        url(r'^day/save/day_cash_sales/$', views.save_cash_sales, name='save_cash_sales'),
                        url(r'^day/save/day_cash_purchase/$', views.save_day_cash_purchase, name='save_cash_purchase'),
                        url(r'^day/save/day_cash_receipt/$', views.save_day_cash_receipt, name='save_cash_receipt'),
