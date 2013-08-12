694e8901bfc23f25db7f552c58697f1559b2771d
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-12 15:43:33 +0545

    refactored day journal to day book in js

---
+++ js/day_book.js
@@ -0,0 +1,164 @@
+function DayBook(data){
+    var self = this;
+    for (var k in data)
+        self[k]=data[k];
+
+    $.ajax({
+        url: '/ledger/accounts/json/',
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            self.accounts = data;
+        }
+    });
+
+    $.ajax({
+        url: '/inventory/items/json/',
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            self.items = data;
+        }
+    });
+
+    var cash_sales_options = {
+        rows: data.cash_sales,
+        save_to_url : '/day_book/save/cash_sales/',
+        properties : {day_book_date : self.date},
+        onSaveSuccess : function(msg, rows){
+            $("#cash-sales > tr").each(function (index) {
+                $($("#cash-sales > tr")[index]).addClass('invalid-row');
+            });
+            for (var i in msg){
+                rows[i].id = msg[i];
+                $($("#cash-sales > tr")[i]).removeClass('invalid-row');
+            }
+        }
+    };
+
+    var cash_purchase_options = {
+        rows: data.cash_purchase,
+        save_to_url : '/day_book/save/cash_purchase/',
+        properties : {day_book_date : self.date},
+        onSaveSuccess : function(msg, rows){
+            $("#cash-purchase > tr").each(function (index) {
+                $($("#cash-purchase > tr")[index]).addClass('invalid-row');
+            });
+            for (var i in msg){
+                rows[i].id = msg[i];
+                $($("#cash-purchase > tr")[i]).removeClass('invalid-row');
+            }
+        }
+    };
+
+    var cash_receipt_options = {
+        rows: data.cash_receipt,
+        save_to_url : '/day_book/save/cash_receipt/',
+        properties : {day_book_date : self.date},
+        onSaveSuccess : function(msg, rows){
+            $("#cash-receipt > tr").each(function (index) {
+                $($("#cash-receipt > tr")[index]).addClass('invalid-row');
+            });
+            for (var i in msg){
+                rows[i].id = msg[i];
+                $($("#cash-receipt > tr")[i]).removeClass('invalid-row');
+            }
+        }
+    };
+
+    var cash_payment_options = {
+        rows: data.cash_payment,
+        save_to_url : '/day_book/save/cash_payment/',
+        properties : {day_book_date : self.date},
+        onSaveSuccess : function(msg, rows){
+            $("#cash-payment > tr").each(function (index) {
+                $($("#cash-payment > tr")[index]).addClass('invalid-row');
+            });
+            for (var i in msg){
+                rows[i].id = msg[i];
+                $($("#cash-payment > tr")[i]).removeClass('invalid-row');
+            }
+        }
+    };
+
+    var summary_cash_options = {
+        rows: data.cash_payment,
+        save_to_url : '/day_book/save/cash_payment/',
+        properties : {day_book_date : self.date},
+        onSaveSuccess : function(msg, rows){
+            $("#summary-cash > tr").each(function (index) {
+                $($("#summary-cash > tr")[index]).addClass('invalid-row');
+            });
+            for (var i in msg){
+                rows[i].id = msg[i];
+                $($("#summary-cash > tr")[i]).removeClass('invalid-row');
+            }
+        }
+    };
+
+    self.cash_sales = new TableViewModel(cash_sales_options, DayCashSalesRow);
+
+    self.cash_purchase = new TableViewModel(cash_purchase_options, DayCashSalesRow);
+
+    self.cash_receipt = new TableViewModel(cash_receipt_options, DayCashReceiptRow);
+
+    self.cash_payment = new TableViewModel(cash_payment_options, DayCashReceiptRow);
+
+    self.summary_cash = new TableViewModel(summary_cash_options, DaySummaryCashRow);
+
+    self.recordItem = function(item, event){
+        item.item_id = get_target(event).data('selected');
+    }
+
+    self.recordAccount = function(item, event){
+        item.account_id = get_target(event).data('selected');
+    }
+}
+
+function DayCashSalesRow(row){
+    var self = this;
+
+    self.item_id = ko.observable();
+    self.quantity = ko.observable();
+    self.amount = ko.observable(0);
+
+    for (var k in row)
+        self[k] = ko.observable(row[k]);
+
+    self.rate = ko.computed(function(){
+        var rate =  self.amount()/self.quantity();
+        return isNaN(rate) ? '' : Math.round(rate*100)/100;
+    });
+
+}
+
+function DayCashReceiptRow(row){
+    var self = this;
+
+    self.account = ko.observable();
+    self.amount = ko.observable(0);
+
+    self.account_id = '';
+
+    for (var k in row)
+        self[k] = ko.observable(row[k]);
+
+}
+
+function DaySummaryCashRow(row){
+    var self = this;
+
+    self.opening = ko.observable(1000);
+    self.inward = ko.computed(function(){
+            return 2000;
+        }
+    );
+    self.outward = ko.observable(0);
+    self.closing = ko.observable();
+    self.actual = ko.observable();
+    self.difference = ko.observable();
+
+    for (var k in row)
+        self[k] = ko.observable(row[k]);
+
+}
