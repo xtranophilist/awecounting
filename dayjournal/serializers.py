e316be1f8fec5f4bc1a1b80ab89a771444f4fcd2
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-13 22:16:17 +0545

    added CreditSales Serializer

--- dayjournal/serializers.py
+++ dayjournal/serializers.py
@@ -14,23 +14,35 @@ class CashSalesSerializer(serializers.ModelSerializer):
 
 class CashPurchaseSerializer(serializers.ModelSerializer):
     account_id = serializers.Field(source='purchase_ledger_id')
+
     class Meta:
         model = CashPurchase
         exclude = ['day_journal', 'purchase_ledger']
 
 
 class CashReceiptSerializer(serializers.ModelSerializer):
+    account_id = serializers.Field(source='received_from_id')
 
     class Meta:
         model = CashReceipt
-        exclude = ['day_journal']
+        exclude = ['day_journal', 'received_from']
 
 
 class CashPaymentSerializer(serializers.ModelSerializer):
+    account_id = serializers.Field(source='payment_to_id')
 
     class Meta:
         model = CashPayment
-        exclude = ['day_journal']
+        exclude = ['day_journal', 'payment_to']
+
+
+class CreditSalesSerializer(serializers.ModelSerializer):
+    account_cr_id = serializers.Field(source='sales_ledger_id')
+    account_dr_id = serializers.Field(source='customer_id')
+
+    class Meta:
+        model = CreditSales
+        exclude = ['day_journal', 'sales_ledger', 'customer']
 
 
 class SummaryCashSerializer(serializers.ModelSerializer):
@@ -45,6 +57,7 @@ class DayJournalSerializer(serializers.ModelSerializer):
     cash_purchase = CashPurchaseSerializer()
     cash_receipt = CashReceiptSerializer()
     cash_payment = CashPaymentSerializer()
+    credit_sales = CreditSalesSerializer()
     summary_cash = SummaryCashSerializer()
 
     class Meta:
