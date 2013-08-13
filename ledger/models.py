e203b49e22595ff0ddd8d2d19c5b96cbae727a04
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-13 16:25:45 +0545

    created hierarchy in Accounts; created Tags for Accounts

--- ledger/models.py
+++ ledger/models.py
@@ -6,7 +6,8 @@ class Account(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
     company = models.ForeignKey(Company)
-    current_balance = models.FloatField()
+    current_balance = models.FloatField(default=0)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
 
     def get_absolute_url(self):
         return '/account/' + str(self.id)
@@ -33,3 +34,9 @@ class Transaction(models.Model):
     amount = models.FloatField()
     current_balance = models.FloatField()
     type = models.CharField(max_length=2)  # Dr. or Cr.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=254, null=True, blank=True)
+    accounts = models.ManyToManyField(Account, related_name='tags', blank=True)
