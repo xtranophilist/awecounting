1c6766e4e643567d2004a28784a1f98914bf15f0
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-13 16:32:45 +0545

    added Ledger models to admin panel

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from models import Account
+from models import Account, InventoryAccount, Tag, Transaction
 
 admin.site.register(Account)
+admin.site.register(InventoryAccount)
+admin.site.register(Tag)
+admin.site.register(Transaction)
