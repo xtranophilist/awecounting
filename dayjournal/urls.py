ae1b39f8fb3a758c71c12dae6b468eac1aad2f0c
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-13 22:24:19 +0545

    added Urls for all Credit blocks

--- dayjournal/urls.py
+++ dayjournal/urls.py
@@ -8,6 +8,10 @@ urlpatterns = patterns('',
                        url(r'^save/cash_purchase/$', views.save_cash_purchase, name='save_cash_purchase'),
                        url(r'^save/cash_receipt/$', views.save_cash_receipt, name='save_cash_receipt'),
                        url(r'^save/cash_payment/$', views.save_cash_payment, name='save_cash_payment'),
+                       url(r'^save/credit_sales/$', views.save_credit_sales, name='save_credit_sales'),
+                       url(r'^save/credit_purchase/$', views.save_credit_purchase, name='save_credit_purchase'),
+                       url(r'^save/credit_expense/$', views.save_credit_expense, name='save_credit_expense'),
+                       url(r'^save/credit_income/$', views.save_credit_income, name='save_credit_income'),
                        # url(r'^day/save/(?P<submodel>[a-zA-Z0-9_.-]+)/$', views.save_submodel, name='save_submodel'),
                        )
 
