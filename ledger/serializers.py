5024f37e3d458398252b65fdb25acfe4531eb9d4
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-13 16:53:32 +0545

    created Tag Serialier

--- ledger/serializers.py
+++ ledger/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from models import Account
+from models import Account, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    tags = serializers.RelatedField(many=True)
+
     class Meta:
         model = Account
+        # exclude = ['code', 'company', 'parent', 'current_balance']
+        fields = ['id', 'name', 'tags']
