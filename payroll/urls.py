34fdd1465b91cdb110cf766eedbc4a514d27f17b
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-19 01:17:39 +0545

    created URLs and views forpayroll entry

--- payroll/urls.py
+++ payroll/urls.py
@@ -3,7 +3,7 @@ import views
 
 urlpatterns = patterns('',
                        # url(r'^invoices/$', views.list_invoice, name='list_invoice'),
-                       url(r'^new$', views.entry, name='new_invoice'),
+                       url(r'^new$', views.entry, name='new_payroll'),
                        url(r'^(?P<id>[0-9]+)/$', views.entry, name='view_payroll'),
                        # url(r'^save/$', views.save_entry, name='save_invoice'),
                        )
--- payroll/views.py
+++ payroll/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from payroll.models import Entry
+from payroll.serializers import EntrySerializer
+
+
+def entry(request, id=None):
+    if id:
+        entry = get_object_or_404(Entry, id=id)
+    else:
+        entry = Entry(company=request.user.company)
+    # form = InvoiceForm(data=request.POST, instance=invoice)
+    data = EntrySerializer(entry).data
+    return render(request, 'entry.html', {'data': data})
