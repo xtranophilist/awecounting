57c7a43f476a139fa6bcc7dd9bb1a2e7da8aacc1
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-20 22:38:14 +0545

    added util function for handling returning of numeric values (sends empty for non-numeric)

--- js/base.js
+++ js/base.js
@@ -41,35 +41,22 @@ function intersect_safe(a, b){
     return result;
 }
 
-function intersection_destructive(a, b)
-{
-  var result = new Array();
-  while( a.length > 0 && b.length > 0 )
-  {
-     if      (a[0] < b[0] ){ a.shift(); }
-     else if (a[0] > b[0] ){ b.shift(); }
-     else /* they're equal */
-     {
-       result.push(a.shift());
-       b.shift();
-     }
-  }
+function intersection(arr1, arr2) {
+    var temp = [];
 
-  return result;
+    for (var i in arr1) {
+        var element = arr1[i];
+
+        if (arr2.indexOf(element) > -1) {
+            temp.push(element);
+        }
+    }
+
+    return temp;
 }
 
-function intersection(arr1, arr2) {
-  var temp = [];
-
-  for (var i in arr1) {
-    var element = arr1[i];
-
-    if (arr2.indexOf(element) > -1) {
-      temp.push(element);
-    }
-  }
-
-  return temp;
+function rnum(o){
+    return isNaN(o)?'':o;
 }
 
 function get_target(e){
