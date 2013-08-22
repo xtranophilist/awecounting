8754f7d79fc24e89c1e532f1735e2b8f50c18c16
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-22 16:35:56 +0545

    reverted related field location between Inventory Item and Inventory Account

--- inventory/models.py
+++ inventory/models.py
@@ -5,50 +5,19 @@ from users.models import Company
 from datetime import date
 from core.models import Tag
 
+
 class Category(models.Model):
     code = models.CharField(max_length=10, blank=True, null=True)
     name = models.CharField(max_length=254)
     description = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
-        return '[' + self.code + '] ' + self.name
-
-
-class Item(models.Model):
-    code = models.CharField(max_length=10)
-    name = models.CharField(max_length=254)
-    description = models.TextField(blank=True, null=True)
-    purchase_price = models.FloatField()
-    purchase_account = models.ForeignKey(Account, related_name='purchase_items')
-    purchase_tax_scheme = models.ForeignKey(TaxScheme, verbose_name=u'Tax Rate', related_name='purchase_items')
-    sales_price = models.FloatField()
-    sales_account = models.ForeignKey(Account, related_name='sales_items')
-    sales_tax_scheme = models.ForeignKey(TaxScheme, verbose_name=u'Tax Rate', related_name='sales_items')
-    company = models.ForeignKey(Company)
-    category = models.ForeignKey(Category)
-
-    def save(self, *args, **kwargs):
-        if self.pk is None:
-            account = InventoryAccount(code=self.ac_no, name=self.name)
-            account.company = self.company
-            account.save()
-            # account.add_tag('Bank')
-            self.account = account
-        super(Item, self).save(*args, **kwargs)
-
-    def add_tag(self, tag):
-        # all_tags = self.get_all_tags()
-        tag_instance, created = Tag.objects.get_or_create(name=tag)
-        self.tags.add(tag_instance)
-
-    def __unicode__(self):
-        return '[' + self.code + '] ' + self.name
+        return '[' + str(self.code) + '] ' + str(self.name)
 
 
 class InventoryAccount(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
-    item = models.ForeignKey(Item)
 
     def get_absolute_url(self):
         return '/inventory_account/' + str(self.id)
@@ -65,6 +34,38 @@ class InventoryAccount(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Item(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=254)
+    description = models.TextField(blank=True, null=True)
+    purchase_price = models.FloatField()
+    purchase_account = models.ForeignKey(Account, related_name='purchase_items')
+    purchase_tax_scheme = models.ForeignKey(TaxScheme, verbose_name=u'Tax Rate', related_name='purchase_items')
+    sales_price = models.FloatField()
+    sales_account = models.ForeignKey(Account, related_name='sales_items')
+    sales_tax_scheme = models.ForeignKey(TaxScheme, verbose_name=u'Tax Rate', related_name='sales_items')
+    company = models.ForeignKey(Company)
+    category = models.ForeignKey(Category)
+    account = models.ForeignKey(InventoryAccount)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            account = InventoryAccount(code=self.code, name=self.name)
+            account.company = self.company
+            account.save()
+            # account.add_tag('Bank')
+            self.account = account
+        super(Item, self).save(*args, **kwargs)
+
+    def add_tag(self, tag):
+        # all_tags = self.get_all_tags()
+        tag_instance, created = Tag.objects.get_or_create(name=tag)
+        self.tags.add(tag_instance)
+
+    def __unicode__(self):
+        return '[' + self.code + '] ' + self.name
 
 
 class InventoryTransaction(models.Model):
