8575d1ccfeedd921cad3d09f3de32b49a077869f
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-22 22:07:29 +0545

    optimised Payroll saving

--- payroll/views.py
+++ payroll/views.py
@@ -32,14 +32,14 @@ def save_entry(request):
     else:
         entry = Entry()
         # if not created:
-    entry = save_model(entry, values)
-    dct['id'] = entry.id
+    if params.get('rows') != [{}] or params.get('deleted_rows') != []:
+        entry = save_model(entry, values)
+        dct['id'] = entry.id
     model = EntryRow
-    print params
     for index, row in enumerate(params.get('rows')):
-        if invalid(row, ['pay_heading', 'account_id', 'amount', 'amount', 'tax']):
+        if invalid(row, ['pay_heading', 'account_id', 'amount', 'tax']):
             continue
-        values = {'sn': index+1, 'employee_id': row.get('account_id'), 'pay_heading': row.get('pay_heading'),
+        values = {'sn': index+1, 'employee_id': row.get('account_id'), 'pay_heading_id': row.get('pay_heading'),
                   'tax': row.get('tax'), 'remarks': row.get('remarks'), 'hours': row.get('hours'),
                   'amount': row.get('amount'), 'entry': entry}
         submodel, created = model.objects.get_or_create(id=row.get('id'), defaults=values)
@@ -47,4 +47,5 @@ def save_entry(request):
             submodel = save_model(submodel, values)
         dct['saved'][index] = submodel.id
     delete_rows(params.get('deleted_rows'), model)
+
     return HttpResponse(json.dumps(dct), mimetype="application/json")
