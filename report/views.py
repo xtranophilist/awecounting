f3e7823811b653c7a60609783334e9f93ff37a84
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-23 22:35:40 +0545

    created base structure for trial balance

--- report/views.py
+++ report/views.py
@@ -1,9 +1,33 @@
 from django.http import HttpResponse
 import json
 from django.shortcuts import render, get_object_or_404, redirect
+from core.models import Tag
+from mptt.templatetags.mptt_tags import cache_tree_children
+
+
+def recursive_node_to_dict(node):
+    result = {
+        'id': node.pk,
+        'name': node.name,
+    }
+    children = [recursive_node_to_dict(c) for c in node.get_children()]
+    if children:
+        result['children'] = children
+    return result
+
+
+def to_dict(model):
+    root_nodes = cache_tree_children(model.objects.all())
+    dicts = []
+    for n in root_nodes:
+        dicts.append(recursive_node_to_dict(n))
+    return dicts
 
 
 def trial_balance(request):
+    tags = Tag.objects.filter(company=request.user.company)
+    dicts = to_dict(Tag)
+    print json.dumps(dicts, indent=4)
     return render(request, 'trial_balance.html', {
-        # 'tax_schemes': tax_schemes
+        'tags': tags
     })
