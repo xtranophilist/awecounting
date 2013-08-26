ed6b8c0a55e5c16e7e8b931569e7624d3bdbe217
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-26 17:47:42 +0545

    fixed TableViewModel

--- js/base.js
+++ js/base.js
@@ -18,18 +18,21 @@ $(document).ready(function () {
 
 //Useful Functions
 
-function compare_arrays(arr1, arr2){
+function compare_arrays(arr1, arr2) {
     return $(arr1).not(arr2).length == 0 && $(arr2).not(arr1).length == 0;
 }
 
-function intersect_safe(a, b){
-    var ai=0, bi=0;
+function intersect_safe(a, b) {
+    var ai = 0, bi = 0;
     var result = new Array();
 
-    while( ai < a.length && bi < b.length )
-    {
-        if      (a[ai] < b[bi] ){ ai++; }
-        else if (a[ai] > b[bi] ){ bi++; }
+    while (ai < a.length && bi < b.length) {
+        if (a[ai] < b[bi]) {
+            ai++;
+        }
+        else if (a[ai] > b[bi]) {
+            bi++;
+        }
         else /* they're equal */
         {
             result.push(a[ai]);
@@ -55,33 +58,33 @@ function intersection(arr1, arr2) {
     return temp;
 }
 
-function rnum(o){
-    return isNaN(o)?'':o;
+function rnum(o) {
+    return isNaN(o) ? '' : o;
 }
 
-function isAN(n){
-    if (n=='')
+function isAN(n) {
+    if (n == '')
         return false;
     return !isNaN(n);
 }
 
-function empty_or_undefined(o){
-    if (o=='' || typeof o =='undefined')
+function empty_or_undefined(o) {
+    if (o == '' || typeof o == 'undefined')
         return true;
     return false;
 }
 
-function empty_to_zero(o){
-    if (o=='' || typeof o =='undefined')
+function empty_to_zero(o) {
+    if (o == '' || typeof o == 'undefined')
         return 0;
     return o;
 }
 
-function get_target(e){
+function get_target(e) {
     return $((e.currentTarget) ? e.currentTarget : e.srcElement); //for IE <9 compatibility
 }
 
-Object.size = function(obj) {
+Object.size = function (obj) {
     var size = 0, key;
     for (key in obj) {
         if (obj.hasOwnProperty(key)) size++;
@@ -92,29 +95,31 @@ Object.size = function(obj) {
 //String
 
 //Converts underscored or dashed string to camelCase
-String.prototype.toCamelCase = function(){
-    return this.replace(/[-_\s]+(.)?/g, function(match, c){ return c ? c.toUpperCase() : ""; });
+String.prototype.toCamelCase = function () {
+    return this.replace(/[-_\s]+(.)?/g, function (match, c) {
+        return c ? c.toUpperCase() : "";
+    });
 }
 
 //Converts camelCased or dashed string into an underscored_one
-String.prototype.toUnderscore = function(){
+String.prototype.toUnderscore = function () {
     return this.replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/[-\s]+/g, '_').toLowerCase();
 }
 
 //Converts underscored or camelCased string into an dashed-one
-String.prototype.toDash  = function(){
+String.prototype.toDash = function () {
     return this.replace(/([A-Z])/g, '-$1').replace(/[-_\s]+/g, '-').toLowerCase();
 }
 
 //Fixes
 
-$(document).on('mouseup mousedown', '[contenteditable]',function(){
+$(document).on('mouseup mousedown', '[contenteditable]', function () {
     this.focus();
 });
 
 //setup ajax requests to include csrf token
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
+    beforeSend: function (xhr, settings) {
         function getCookie(name) {
             var cookieValue = null;
             if (document.cookie && document.cookie != '') {
@@ -130,6 +135,7 @@ $.ajaxSetup({
             }
             return cookieValue;
         }
+
         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
             // Only send the token to relative URLs i.e. locally.
             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
@@ -139,72 +145,81 @@ $.ajaxSetup({
 
 //Reusable KO Models
 
-function TableViewModel(options, row_model){
+function TableViewModel(options, row_model) {
 
     var self = this;
 
-    if (typeof(options.properties)!='undefined'){
+    if (typeof(options.properties) != 'undefined') {
         /** @namespace options.properties */
         for (var k in options.properties)
-            self[k]=options.properties[k];
+            self[k] = options.properties[k];
     }
 
     self.message = ko.observable();
-    self.status = ko.observable('standby')
+    self.status = ko.observable('standby');
 
-    self.rows = ko.observableArray(ko.utils.arrayMap(options.rows, function(item) {
-        return new row_model(item);
-    }));
 
-    //if there are any rows
-    if(self.rows().length){
-        //if row has a sn() field, sort it
-        if (typeof self.rows()[0].sn != 'undefined'){
-            self.rows().sort(function (l, r) {
-                return l.sn() > r.sn() ? 1 : -1
-            });
+    if (typeof row_model != 'undefined') {
+        self.rows = ko.observableArray(ko.utils.arrayMap(options.rows, function (item) {
+            return new row_model(item);
+        }));
+
+        //if there are any rows
+        if (self.rows().length) {
+            //if row has a sn() field, sort it
+            if (typeof self.rows()[0].sn != 'undefined') {
+                self.rows().sort(function (l, r) {
+                    return l.sn() > r.sn() ? 1 : -1
+                });
+            }
         }
+
+        self.deleted_rows = [];
+
+        self.hasNoRows = ko.computed(function () {
+            return self.rows().length === 0;
+        });
+
+        self.addRow = function () {
+            var new_item_index = self.rows().length + 1;
+            self.rows.push(new row_model());
+        };
+
+        self.removeRow = function (row) {
+            self.rows.remove(row);
+            self.deleted_rows.push(row);
+        };
+
+        if (self.hasNoRows()) {
+            self.addRow();
+        }
+
+        self._initial_rows = self.rows().slice(0);
+
+        self.reset = function () {
+            self.rows(self._initial_rows);
+        }
+
     }
 
-    self.deleted_rows  = [];
 
-    self.hasNoRows = ko.computed(function(){
-        return self.rows().length === 0;
-    });
-
-    self.addRow = function() {
-        var new_item_index = self.rows().length+1;
-        self.rows.push(new row_model());
-    };
-
-    self.removeRow = function(row) {
-        self.rows.remove(row);
-        self.deleted_rows.push(row);
-    };
-
-    if (self.hasNoRows()){
-        self.addRow();
-    }
-
-    self._initial_rows = self.rows().slice(0);
-
-    if (typeof(options.save_to_url) != 'undefined'){
-        self.save = function(model, e){
+    if (typeof(options.save_to_url) != 'undefined') {
+        self.save = function (model, e) {
             self.status('waiting');
             var el = get_target(e);
             $.ajax({
                 type: "POST",
                 url: options.save_to_url,
                 data: ko.toJSON(self),
-                success: function(msg){
+                success: function (msg) {
                     self.message('Saved!');
-                    if (typeof(options.onSaveSuccess) != 'undefined'){
+                    if (typeof(options.onSaveSuccess) != 'undefined') {
                         options.onSaveSuccess(msg, self.rows());
                     }
                     self.status('success');
-                    self.deleted_rows  = [];
+                    self.deleted_rows = [];
                 },
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
                     self.message('Saving Failed!');
                     self.status('error');
                 }
@@ -212,13 +227,11 @@ function TableViewModel(options, row_model){
 
         }
     }
-    else{
-        self.save= function(){
+    else {
+        self.save = function () {
             throw new Error("'save_to_url' option not passed to TableViewModel or save() not implemented!");
         }
     }
 
-    self.reset= function(){
-        self.rows(self._initial_rows);
-    }
+
 }
