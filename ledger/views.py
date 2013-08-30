5d47035369ca3dc0916486c1dc16dec11398bb5d
Dipesh Acharya <xtranophilist@gmail.com>
2013-08-30 14:57:32 +0545

    db query optimisation with prefetch related

--- ledger/views.py
+++ ledger/views.py
@@ -1,4 +1,4 @@
-from ledger.models import Account
+from ledger.models import Account, JournalEntry
 from ledger.serializers import AccountSerializer
 from django.http import HttpResponse
 import json
@@ -56,8 +56,12 @@ def view_account(request, id):
     account = get_object_or_404(Account, id=id)
     # transactions = account.transactions
     base_template = 'dashboard.html'
+    journal_entries = JournalEntry.objects.filter(transactions__account_id=account.id).order_by('id',
+                                                                                                'date').prefetch_related(
+        'transactions', 'transactions__account').select_related()
     return render(request, 'view_account.html', {
         'account': account,
         # 'transactions': transactions.all(),
+        'journal_entries': journal_entries,
         'base_template': base_template,
     })
