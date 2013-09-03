1b95708d56f8d6b531c0290a8bccab50c2e3c403
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-03 17:36:18 +0545

    created bank block in dashboard index

--- bank/urls.py
+++ bank/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-                       url(r'^account/new/$', views.bank_account_form, name='create_bank_account'),
+                       # url(r'^accounts/$', views.bank_accounts, name='list_bank_accounts'),
+                       url(r'^account/create/$', views.bank_account_form, name='create_bank_account'),
                        url(r'^cheque-deposit/$', views.cheque_deposit, name='new_cheque_deposit'),
                        url(r'^cheque-deposit/(?P<id>[0-9]+)$', views.cheque_deposit, name='view_cheque_deposit'),
                        url(r'^cash-deposit/$', views.cash_deposit, name='new_cash_deposit'),
