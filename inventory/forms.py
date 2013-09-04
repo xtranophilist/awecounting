e273173c7dd5ef4f590f06731ac1f378d6ec9f88
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-05 02:19:17 +0545

    inventory items form

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,9 +1,18 @@
 from acubor.lib import KOModelForm
 from django import forms
 from models import Item
+from ledger.models import Account, Category
+from tax.models import TaxScheme
 
 
 class ItemForm(KOModelForm):
+    def __init__(self, *args, **kwargs):
+        company = kwargs.pop('company', None)
+        super(ItemForm, self).__init__(*args, **kwargs)
+        self.fields['purchase_account'].queryset = Account.objects.filter(company=company, category__name='Bank')
+        self.fields['sales_account'].queryset = Account.objects.filter(company=company)
+        self.fields['sales_tax_scheme'].queryset = TaxScheme.objects.filter(company=company)
+        self.fields['category'].queryset = Category.objects.filter(company=company)
 
     class Meta:
         model = Item
