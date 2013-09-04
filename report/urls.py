8656027e09e55cc6999cf5cb73201e2146aea9cc
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-04 08:43:09 +0545

    breadcrumb for trial balance

--- report/urls.py
+++ report/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^trial-balance/$', views.trial_balance, name='trial_balance'),
-    )
+                       url(r'^trial-balance/$', views.trial_balance, name='trial_balance'),
+)
 
