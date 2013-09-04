b82577c8954b97b97e2a6dbc07cb97f31f52ae9d
RohitRox <link2j.roy@gmail.com>
2013-09-05 00:27:16 +0545

    dashboard ui fixes

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
-    $('.date-picker').datepicker();
+		$('.date-picker').datepicker({autoclose: true});
 
     $('#day-journal-selection-button').on('click', function () {
         window.location = '/day/' + $('#day-journal-date').val();
     });
+
+		$(function(){
+		  $('.flip-container').mouseenter(function(){
+		      $(this).find('.flip-body').addClass('open');
+		  });
+		  $('.flip-container').mouseleave(function(){
+		      if(!$('.datepicker').is(":visible")){
+		          $(this).find('.flip-body').removeClass('open');
+		      }
+		  });
+		});
+
 });
 
 
