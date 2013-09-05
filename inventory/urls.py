e70e6c2253a65fed419956996c361dc4d46efd6e
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-05 21:16:58 +0545

    cleaned all urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,7 +4,6 @@ import views
 urlpatterns = patterns('',
                        url(r'^items/$', views.list_all_items, name='list_all_items'),
                        url(r'^create/$', views.item_form, name='create_inventory_item'),
-                       # url(r'^$', views.ListItem.as_view(template_name='list.html'), name='list_items'),
                        url(r'^items/json/$', views.items_as_json, name='items_as_json'),
                        url(r'^accounts/json/$', views.accounts_as_json, name='accounts_as_json'),
                        url(r'^accounts/(?P<day>\d{4}-\d{2}-\d{2}).json$', views.accounts_by_day_as_json,
--- ledger/urls.py
+++ ledger/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-                       # url(r'^create/$', views.CreateAccount.as_view(template_name='create_form.html'), name='create_account'),
                        url(r'^accounts/json/$', views.accounts_as_json, name='accounts_as_json'),
-                       # url(r'^$', views.ListAccount.as_view(template_name='list.html'), name='list_account'),
-                       # url(r'^(?P<pk>[0-9]+)/$', views.DetailAccount.as_view(template_name='detail.html'), name='detail_view'),
                        url(r'^$', views.list_accounts, name='list_account'),
                        url(r'^create/$', views.account_form, name='create_account'),
                        url(r'^(?P<id>[0-9]+)/update/$', views.account_form, name='update_account'),
