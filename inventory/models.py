51149a0853bfd069920e8c63763746a18d84a31d
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-05 15:04:14 +0545

    new models, tables for inventory

--- inventory/models.py
+++ inventory/models.py
@@ -3,6 +3,12 @@ from tax.models import TaxScheme
 from ledger.models import Account
 from users.models import Company
 from datetime import date
+from acubor.lib import zero_for_none, none_for_zero
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+from django.db.models import F
+from datetime import date, timedelta
 
 
 class Category(models.Model):
@@ -22,18 +28,61 @@ class InventoryAccount(models.Model):
     def get_absolute_url(self):
         return '/inventory_account/' + str(self.id)
 
-    def get_last_day_last_transaction(self):
-        transactions = InventoryTransaction.objects.filter(account=self, date__lt=date.today()).order_by('-id', '-date')[:1]
+    def get_day_opening_dr(self, before_date=None):
+        if not before_date:
+            before_date = date.today()
+        transactions = Transaction.objects.filter(account=self, journal_entry__date__lt=before_date).order_by(
+            '-journal_entry__id', '-journal_entry__date')[:1]
         if len(transactions) > 0:
-            return transactions[0]
+            return transactions[0].current_dr
+        return self.current_dr
 
-    def get_last_transaction_before(self, before_date):
-        transactions = InventoryTransaction.objects.filter(account=self, date__lt=before_date).order_by('-id', '-date')[:1]
+    def get_day_opening_cr(self, before_date=None):
+        if not before_date:
+            before_date = date.today()
+        transactions = Transaction.objects.filter(account=self, journal_entry__date__lt=before_date).order_by(
+            '-journal_entry__id', '-journal_entry__date')[:1]
         if len(transactions) > 0:
-            return transactions[0]
+            return transactions[0].current_cr
+        return self.current_cr
 
-    def __unicode__(self):
-        return self.name
+    def get_day_opening(self, before_date=None):
+        if self.name == 'Cash':
+            print self.get_day_opening_dr(before_date)
+            print self.get_day_opening_cr(before_date)
+        return zero_for_none(self.get_day_opening_dr(before_date)) - zero_for_none(self.get_day_opening_cr(before_date))
+
+    # day_opening_dr = property(get_day_opening_dr)
+    # day_opening_cr = property(get_day_opening_cr)
+    #
+    # day_opening = property(get_day_opening)
+
+    def add_category(self, category):
+        # all_categories = self.get_all_categories()
+        category_instance, created = Category.objects.get_or_create(name=category, company=self.company)
+        # self.categories.add(category_instance)
+        self.category = category_instance
+
+    def get_all_categories(self):
+        return self.category.get_ancestors(include_self=True)
+
+    categories = property(get_all_categories)
+
+    def get_cr_amount(self, day):
+        #journal_entry= JournalEntry.objects.filter(date__lt=day,transactions__account=self).order_by('-id','-date')[:1]
+        transactions = Transaction.objects.filter(journal_entry__date__lt=day, account=self).order_by(
+            '-journal_entry__id', '-journal_entry__date')[:1]
+        if len(transactions) > 0:
+            return transactions[0].current_cr
+        return 0
+
+    def get_dr_amount(self, day):
+        #journal_entry= JournalEntry.objects.filter(date__lt=day,transactions__account=self).order_by('-id','-date')[:1]
+        transactions = Transaction.objects.filter(journal_entry__date__lt=day, account=self).order_by(
+            '-journal_entry__id', '-journal_entry__date')[:1]
+        if len(transactions) > 0:
+            return transactions[0].current_dr
+        return 0
 
 
 class Item(models.Model):
@@ -68,10 +117,134 @@ class Item(models.Model):
         return '[' + self.code + '] ' + self.name
 
 
-class InventoryTransaction(models.Model):
-    account = models.ForeignKey(InventoryAccount, related_name='transactions')
+class JournalEntry(models.Model):
     date = models.DateField()
-    quantity = models.FloatField()
-    type = models.CharField(max_length=3)  # in or out
-    current_quantity = models.FloatField()
+    content_type = models.ForeignKey(ContentType, related_name='inventory_journal_entries')
+    model_id = models.IntegerField()
 
+    def __str__(self):
+        return str(self.content_type) + ': ' + str(self.model_id) + ' [' + str(self.date) + ']'
+
+    class Meta:
+        verbose_name_plural = u'InventoryJournal Entries'
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey(InventoryAccount)
+    dr_amount = models.FloatField(null=True, blank=True)
+    cr_amount = models.FloatField(null=True, blank=True)
+    current_dr = models.FloatField(null=True, blank=True)
+    current_cr = models.FloatField(null=True, blank=True)
+    journal_entry = models.ForeignKey(JournalEntry, related_name='inventory_transactions')
+
+    def get_balance(self):
+        return zero_for_none(self.current_dr) - zero_for_none(self.current_cr)
+
+    def __str__(self):
+        return str(self.account) + ' [' + str(self.dr_amount) + ' / ' + str(self.cr_amount) + ']'
+
+
+def alter(account, date, dr_difference, cr_difference):
+    print 'altering for' + str(account)
+    print dr_difference
+    Transaction.objects.filter(journal_entry__date__gt=date, account=account).update(
+        current_dr=none_for_zero(zero_for_none(F('current_dr')) + zero_for_none(dr_difference)),
+        current_cr=none_for_zero(zero_for_none(F('current_cr')) + zero_for_none(cr_difference)))
+
+
+def set_transactions(submodel, date, *args):
+    # [transaction.delete() for transaction in submodel.transactions.all()]
+    # args = [arg for arg in args if arg is not None]
+    journal_entry, created = JournalEntry.objects.get_or_create(
+        content_type=ContentType.objects.get_for_model(submodel), model_id=submodel.id,
+        defaults={
+            'date': date
+        })
+    for arg in args:
+        # transaction = Transaction(account=arg[1], dr_amount=arg[2])
+        matches = journal_entry.transactions.filter(account=arg[1])
+        if not matches:
+            transaction = Transaction()
+            transaction.account = arg[1]
+            if arg[0] == 'dr':
+                transaction.dr_amount = float(arg[2])
+                transaction.cr_amount = None
+                transaction.account.current_dr = none_for_zero(
+                    zero_for_none(transaction.account.current_dr) + transaction.dr_amount)
+                alter(arg[1], date, float(arg[2]), 0)
+            if arg[0] == 'cr':
+                transaction.cr_amount = float(arg[2])
+                transaction.dr_amount = None
+                transaction.account.current_cr = none_for_zero(
+                    zero_for_none(transaction.account.current_cr) + transaction.cr_amount)
+                alter(arg[1], date, 0, float(arg[2]))
+            transaction.current_dr = none_for_zero(
+                zero_for_none(transaction.account.get_dr_amount(date + timedelta(days=1)))
+                + zero_for_none(transaction.dr_amount))
+            transaction.current_cr = none_for_zero(
+                zero_for_none(transaction.account.get_cr_amount(date + timedelta(days=1)))
+                + zero_for_none(transaction.cr_amount))
+        else:
+            transaction = matches[0]
+            transaction.account = arg[1]
+
+            # cancel out existing dr_amount and cr_amount from current_dr and current_cr
+            # if transaction.dr_amount:
+            #     transaction.current_dr -= transaction.dr_amount
+            #     transaction.account.current_dr -= transaction.dr_amount
+            #
+            # if transaction.cr_amount:
+            #     transaction.current_cr -= transaction.cr_amount
+            #     transaction.account.current_cr -= transaction.cr_amount
+
+            # save new dr_amount and add it to current_dr/cr
+            if arg[0] == 'dr':
+                dr_difference = float(arg[2]) - zero_for_none(transaction.dr_amount)
+                cr_difference = zero_for_none(transaction.cr_amount) * -1
+                alter(arg[1], transaction.journal_entry.date, dr_difference, cr_difference)
+                transaction.dr_amount = float(arg[2])
+                transaction.cr_amount = None
+            else:
+                cr_difference = float(arg[2]) - zero_for_none(transaction.cr_amount)
+                dr_difference = zero_for_none(transaction.dr_amount) * -1
+                alter(arg[1], transaction.journal_entry.date, dr_difference, cr_difference)
+                transaction.cr_amount = float(arg[2])
+                transaction.dr_amount = None
+
+            transaction.current_dr = none_for_zero(zero_for_none(transaction.current_dr) + dr_difference)
+            transaction.current_cr = none_for_zero(zero_for_none(transaction.current_cr) + cr_difference)
+            transaction.account.current_dr = none_for_zero(
+                zero_for_none(transaction.account.current_dr) + dr_difference)
+            transaction.account.current_cr = none_for_zero(
+                zero_for_none(transaction.account.current_cr) + cr_difference)
+
+        #the following code lies outside if,else block, inside for loop
+        transaction.account.save()
+        journal_entry.transactions.add(transaction)
+
+
+def delete_rows(rows, model):
+    for row in rows:
+        if row.get('id'):
+            instance = model.objects.get(id=row.get('id'))
+            JournalEntry.objects.get(content_type=ContentType.objects.get_for_model(model),
+                                     model_id=instance.id).delete()
+            instance.delete()
+
+
+@receiver(pre_delete, sender=Transaction)
+def _transaction_delete(sender, instance, **kwargs):
+    transaction = instance
+    print transaction
+    # cancel out existing dr_amount and cr_amount from account's current_dr and current_cr
+    if transaction.dr_amount:
+        transaction.account.current_dr -= transaction.dr_amount
+
+    if transaction.cr_amount:
+        transaction.account.current_cr -= transaction.cr_amount
+
+    print transaction.dr_amount
+    alter(transaction.account, transaction.journal_entry.date, float(zero_for_none(transaction.dr_amount)) * -1,
+          float(zero_for_none(transaction.cr_amount)) * -1)
+
+    transaction.account.save()
