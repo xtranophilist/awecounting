814cda29b697f3d675bbe0ef85e5b20e6bca0add
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-07 02:28:03 +0545

    gunicorn log goes to log folder

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-errorlog = 'gunicorn.log'
+errorlog = '../log/gunicorn.log'
 #accesslog = 'gunicorn-access.log'
 #loglevel = 'debug'
 proc_name = 'gunicorn_acubor'
