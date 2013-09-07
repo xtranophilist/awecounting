b026cb59d3a845163a9436f388d93d085ea07261
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-07 18:51:10 +0545

    moved MEDIA_ROOT and MEDIA_URL to settings_secret

--- acubor/settings_secret_sample.py
+++ acubor/settings_secret_sample.py
@@ -2,6 +2,15 @@
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = 'http://localhost/acubor_static/'
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = '/var/www/html/acubor_static/media'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = 'http://localhost/acubor_static/media/'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
