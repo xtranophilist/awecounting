0dbbe62929d9450491cf438c89264e04aea5e4c3
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-10 20:22:26 +0545

    fixed #26

--- inventory/forms.py
+++ inventory/forms.py
@@ -13,9 +13,9 @@ class ItemForm(KOModelForm):
     def __init__(self, *args, **kwargs):
         company = kwargs.pop('company', None)
         super(ItemForm, self).__init__(*args, **kwargs)
-        self.fields['purchase_account'].queryset = Account.objects.filter(company=company)
+        self.fields['purchase_account'].queryset = Account.objects.filter(company=company, category__name='Purchase')
         self.fields['purchase_tax_scheme'].queryset = TaxScheme.objects.filter(company=company)
-        self.fields['sales_account'].queryset = Account.objects.filter(company=company)
+        self.fields['sales_account'].queryset = Account.objects.filter(company=company, category__name='Sales')
         self.fields['sales_tax_scheme'].queryset = TaxScheme.objects.filter(company=company)
         self.fields['category'].queryset = Category.objects.filter(company=company)
 
