8432b882b9f2f3b69a67626781f94a11ac000a08
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-14 21:42:15 +0545

    custom toggle ko binding

--- js/ko_lib.js
+++ js/ko_lib.js
@@ -1,8 +1,21 @@
-
 //Custom Bindings
 
+ko.bindingHandlers.toggle = {
+    init: function (element, valueAccessor) {
+        ko.utils.registerEventHandler(element, 'click', function (event) {
+            var toggleValue = valueAccessor();
+            toggleValue(!toggleValue());
+            if (event.preventDefault)
+                event.preventDefault();
+            event.returnValue = false;
+        });
+    },
+    update: function (element, valueAccessor) {
+    }
+};
+
 ko.bindingHandlers.select2 = {
-    init: function(element, valueAccessor, allBindingsAccessor) {
+    init: function (element, valueAccessor, allBindingsAccessor) {
         var obj = valueAccessor(),
             allBindings = allBindingsAccessor(),
             lookupKey = allBindings.lookupKey;
@@ -10,16 +23,16 @@ ko.bindingHandlers.select2 = {
         $(element).select2(obj);
         if (lookupKey) {
             var value = ko.utils.unwrapObservable(allBindings.value);
-            $(element).select2('data', ko.utils.arrayFirst(obj.data.results, function(item) {
+            $(element).select2('data', ko.utils.arrayFirst(obj.data.results, function (item) {
                 return item[lookupKey] === value;
             }));
         }
 
-        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
             $(element).select2('destroy');
         });
     },
-    update: function(element) {
+    update: function (element) {
         $(element).trigger('change');
     }
 };
@@ -30,21 +43,21 @@ ko.bindingHandlers.typeahead = {
         el.attr("autocomplete", "off")
             .typeahead({
                 minLength: 0,
-                source: function(query, process) {
+                source: function (query, process) {
                     var objects = [];
                     map = {};
                     var data = ko.utils.unwrapObservable(valueAccessor());
-                    $.each(data, function(i, object) {
+                    $.each(data, function (i, object) {
                         map[object.name] = object;
                         objects.push(object.name);
                     });
                     process(objects);
                 },
-                updater: function(element){
-                    if(map[element]){
-                        $(el).attr('data-selected',map[element].id);
+                updater: function (element) {
+                    if (map[element]) {
+                        $(el).attr('data-selected', map[element].id);
                         return element;
-                    }else{
+                    } else {
                         return "";
                     }
                 }
@@ -53,34 +66,34 @@ ko.bindingHandlers.typeahead = {
 };
 
 ko.bindingHandlers.flash = {
-    init: function(element) {
+    init: function (element) {
         $(element).hide().fadeIn('slow');
     }
 };
 
 
 ko.bindingHandlers.editableText = {
-    init: function(element, valueAccessor) {
+    init: function (element, valueAccessor) {
         $(element).attr('contenteditable', true);
-        $(element).on('blur', function() {
+        $(element).on('blur', function () {
             var observable = valueAccessor();
-            if (typeof (observable) == 'function'){
-                observable( $(this).text() );
+            if (typeof (observable) == 'function') {
+                observable($(this).text());
             }
         });
     },
-    update: function(element, valueAccessor) {
+    update: function (element, valueAccessor) {
         var value = ko.utils.unwrapObservable(valueAccessor());
         $(element).text(value);
     }
 };
 
 ko.bindingHandlers.numeric = {
-    init: function(element, valueAccessor) {
-        $(element).on('keydown', function(event) {
+    init: function (element, valueAccessor) {
+        $(element).on('keydown', function (event) {
 
             // Allow: backspace, delete, tab, escape, and enter
-            if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+            if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
                 // Allow: Ctrl combinations
                 (event.ctrlKey === true) ||
                 //Allow decimal symbol (.)
@@ -98,16 +111,16 @@ ko.bindingHandlers.numeric = {
             }
         });
     },
-    update: function(element, valueAccessor) {
+    update: function (element, valueAccessor) {
     }
 };
 
 //Custom Observable Extensions
-ko.extenders.numeric = function(target, precision) {
+ko.extenders.numeric = function (target, precision) {
     //create a writeable computed observable to intercept writes to our observable
     var result = ko.computed({
         read: target,  //always return the original observables value
-        write: function(newValue) {
+        write: function (newValue) {
             var current = target(),
                 roundingMultiplier = Math.pow(10, precision),
                 newValueAsNum = isNaN(newValue) ? current : parseFloat(+newValue),
