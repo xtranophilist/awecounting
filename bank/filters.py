96cb1ea96b0e4cb88f59804dd47c1e3868523bf5
acubor <abinash.koirala@acubor.com>
2013-09-18 23:08:47 +0545

    Added Electronic Fund Transfers

--- bank/filters.py
+++ bank/filters.py
@@ -1,6 +1,6 @@
 __author__ = 'xtranophilist'
 import django_filters
-from bank.models import BankCashDeposit, ChequePayment, ChequeDeposit
+from bank.models import BankCashDeposit, ChequePayment, ChequeDeposit, ElectronicFundTransferOut, ElectronicFundTransferIn, ElectronicFundTransferInRow
 from acubor import filter_extra
 from ledger.models import Account
 
@@ -47,3 +47,32 @@ class ChequePaymentFilter(django_filters.FilterSet):
     class Meta:
         model = ChequePayment
         fields = ['bank_account', 'cheque_number', 'date', 'beneficiary', 'amount']
+
+class ElectronicFundTransferInFilter(django_filters.FilterSet):
+    date = filter_extra.DateRangeFilter(label='Date Range')
+    clearing_date = filter_extra.DateRangeFilter(label='Clearing Date Range')
+
+    def __init__(self, *args, **kwargs):
+        company = kwargs.pop('company', None)
+        super(ElectronicFundTransferInFilter, self).__init__(*args, **kwargs)
+        self.filters['bank_account'].field.queryset = Account.objects.filter(company=company, category__name='Bank')
+        self.filters['benefactor'].field.queryset = Account.objects.filter(company=company)
+
+    class Meta:
+        model = ElectronicFundTransferIn
+        fields = ['bank_account', 'date', 'clearing_date', 'benefactor']
+
+
+class ElectronicFundTransferOutFilter(django_filters.FilterSet):
+    transaction_number = django_filters.CharFilter(lookup_type='icontains')
+    date = filter_extra.DateRangeFilter(label='Date Range')
+
+    def __init__(self, *args, **kwargs):
+        company = kwargs.pop('company', None)
+        super(ElectronicFundTransferOutFilter, self).__init__(*args, **kwargs)
+        self.filters['bank_account'].field.queryset = Account.objects.filter(company=company, category__name='Bank')
+        self.filters['beneficiary'].field.queryset = Account.objects.filter(company=company)
+
+    class Meta:
+        model = ElectronicFundTransferOut
+        fields = ['bank_account', 'transaction_number', 'date', 'beneficiary', 'amount']
