be756f1df0f15d4cfda126beb280302a3ec5ed57
acubor <abinash.koirala@acubor.com>
2013-09-18 17:48:01 +0000

    bank.js added EFT related functions.

--- js/bank.js
+++ js/bank.js
@@ -25,6 +25,26 @@ function ChequeReceiptViewModel(data) {
 
 }
 
+function ElectronicFundReceiptViewModel(data) {
+    var self = this;
+
+    self.particulars = new TableViewModel({rows: data.rows}, ElectronicFundPurchaseRow);
+
+    for (var k in data)
+        self[k] = ko.observable(data[k]);
+
+    self.grand_total = function () {
+        var total = 0;
+        $.each(self.particulars.rows(), function () {
+            if (isAN(this.amount()))
+                total += parseFloat(this.amount());
+        });
+        return rnum(total);
+    }
+
+
+}
+
 
 function InvoiceViewModel(data) {
     var self = this;
@@ -119,6 +139,22 @@ function ChequePurchaseRow(row) {
     //default values
     self.cheque_number = ko.observable();
     self.cheque_date = ko.observable();
+    self.drawee_bank = ko.observable();
+    self.drawee_bank_address = ko.observable();
+    self.amount = ko.observable();
+
+    for (var k in row) {
+        if (row[k] != null)
+            self[k] = ko.observable(row[k]);
+    }
+}
+
+function ElectronicFundPurchaseRow(row) {
+
+    var self = this;
+    //default values
+    self.transaction_number = ko.observable();
+    self.transaction_date = ko.observable();
     self.drawee_bank = ko.observable();
     self.drawee_bank_address = ko.observable();
     self.amount = ko.observable();
