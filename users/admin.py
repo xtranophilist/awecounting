41a97b7df5d8c44751d6a0ad7d7db0002c1e7803
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-06 17:57:42 +0545

    added Roles to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 
-from models import User, Company
+from models import User, Company, Role
 
 
 admin.site.register(User)
 admin.site.register(Company)
 admin.site.unregister(Site)
 
-admin.site.unregister(Group)
-admin.site.register(Group)
+admin.site.register(Role)
