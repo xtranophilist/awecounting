cbae230a7191acd67e76cd625e99b63a3b85fb2f
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-09 14:12:15 +0545

    added vouchers to admin

--- voucher/admin.py
+++ voucher/admin.py
@@ -1,1 +1,6 @@
+from django.contrib import admin
+from voucher.models import Invoice, PurchaseVoucher, JournalVoucher
 
+admin.site.register(Invoice)
+admin.site.register(PurchaseVoucher)
+admin.site.register(JournalVoucher)
