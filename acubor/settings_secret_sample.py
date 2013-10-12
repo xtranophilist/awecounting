65ee5a56638ccda9e74626f20923fb76637a9f1a
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-12 15:41:55 +0545

    changed acubor static path

--- acubor/settings_secret_sample.py
+++ acubor/settings_secret_sample.py
@@ -1,15 +1,15 @@
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = 'http://localhost/acubor_static/'
+STATIC_URL = 'http://localhost/static/acubor/'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/var/www/html/acubor_static/media'
+MEDIA_ROOT = '/var/www/html/static/acubor/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://localhost/acubor_static/media/'
+MEDIA_URL = 'http://localhost/static/acubor/media/'
 
 DATABASES = {
     'default': {
