40187a140ba2d6dea9480cc7e5ff6131469af513
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-18 17:05:51 +0545

    frontend create/edit/delete of blog items

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,10 @@ from blog import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.list_blog_entries, name='list_blog_entries'),
-                       url(r'^author/(?P<username>[a-z0-9_]+)/$', views.view_blogs_by_author, name='view_blogs_by_author'),
+                       url(r'^author/(?P<username>[a-z0-9_]+)/$', views.view_blogs_by_author,
+                           name='view_blogs_by_author'),
                        url(r'^(?P<id>[0-9]+)/$', views.view_blog_entry, name='view_blog_entry'),
+                       url(r'^create/$', views.blog_entry_form, name='create_blog_entry'),
+                       url(r'^/update/(?P<id>[0-9]+)$', views.blog_entry_form, name='update_blog_entry'),
+                       url(r'^/delete/(?P<id>[0-9]+)$', views.delete_blog_entry, name='delete_blog_entry'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Blog
+from blog.forms import BlogForm
 
 try:
     from django.contrib.auth import get_user_model
@@ -22,4 +25,34 @@ def view_blog_entry(request, id):
 def view_blogs_by_author(request, username):
     user = User.objects.get(username=username)
     objs = Blog.objects.filter(author=user)
-    return render(request, 'view_blogs_by_author.html', {'objs': objs, 'user': user })
+    return render(request, 'view_blogs_by_author.html', {'objs': objs, 'user': user})
+
+
+@login_required
+def blog_entry_form(request, id=None):
+    if id:
+        obj = get_object_or_404(Blog, id=id, author=request.user)
+        scenario = 'Update'
+    else:
+        obj = Blog()
+        scenario = 'Create'
+    if request.POST:
+        form = BlogForm(data=request.POST, instance=obj)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.author = request.user
+            item.save()
+            return redirect(reverse('list_blog_entries'))
+    else:
+        form = BlogForm(instance=obj)
+    return render(request, 'blog_form.html', {
+        'scenario': scenario,
+        'form': form,
+    })
+
+
+@login_required
+def delete_blog_entry(request, id):
+    obj = get_object_or_404(Blog, id=id, author=request.user)
+    obj.delete()
+    return redirect(reverse('list_blog_entries'))
