ba7ab8bf6212a79ef9010c48745032cba5c60c62
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-18 16:35:24 +0545

    viewing blog posts by user

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from blog import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.list_blog_entries, name='list_blog_entries'),
+                       url(r'^author/(?P<username>[a-z0-9_]+)/$', views.view_blogs_by_author, name='view_blogs_by_author'),
                        url(r'^(?P<id>[0-9]+)/$', views.view_blog_entry, name='view_blog_entry'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Blog
 
+try:
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
+
 
 def list_blog_entries(request):
     objs = Blog.objects.all()
@@ -10,3 +17,9 @@ def list_blog_entries(request):
 def view_blog_entry(request, id):
     obj = get_object_or_404(Blog, id=id)
     return render(request, 'view_blog_entry.html', {'obj': obj})
+
+
+def view_blogs_by_author(request, username):
+    user = User.objects.get(username=username)
+    objs = Blog.objects.filter(author=user)
+    return render(request, 'view_blogs_by_author.html', {'objs': objs, 'user': user })
