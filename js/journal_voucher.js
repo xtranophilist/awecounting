9b13804e428bd3edcabdaaa340be4acfc104a08e
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-19 17:31:07 +0545

    fixed journal voucher ko model as per new design

--- js/journal_voucher.js
+++ js/journal_voucher.js
@@ -4,54 +4,54 @@ $(document).ready(function () {
 });
 
 
-function JournalVoucher(data){
+function JournalVoucher(data) {
     var self = this;
 
     self.date = '';
 
     for (var k in data)
-        self[k]=data[k];
+        self[k] = data[k];
 
     $.ajax({
-        url: '/ledger/accounts/json',
+        url: '/ledger/accounts/json/',
         dataType: 'json',
         async: false,
-        success: function(data) {
+        success: function (data) {
             self.accounts = data;
         }
     });
 
-    var validate = function(msg, rows, tr_wrapper_id){
+    var validate = function (msg, rows, tr_wrapper_id) {
         var selection = $("#" + tr_wrapper_id + " > tr");
         selection.each(function (index) {
             $(selection[index]).addClass('invalid-row');
         });
-        for (var i in msg['saved']){
-            rows[i].id = msg['saved'][''+i+''];
+        for (var i in msg['saved']) {
+            rows[i].id = msg['saved']['' + i + ''];
             $(selection[i]).removeClass('invalid-row');
         }
         var model = self[tr_wrapper_id.toUnderscore()];
-        var saved_size = Object.size(msg['saved']) ;
-        if(saved_size==rows.length)
+        var saved_size = Object.size(msg['saved']);
+        if (saved_size == rows.length)
             model.message('Saved!');
-        else if(saved_size==0){
+        else if (saved_size == 0) {
             model.message('No rows saved!');
             model.status('error');
         }
-        else if(saved_size<rows.length){
-            var message = saved_size.toString() +' row' + ((saved_size==1)?'':'s') + ' saved! ';
-            message += (rows.length-saved_size).toString() +' row' + ((rows.length-saved_size==1)?' is':'s are') + ' incomplete!';
+        else if (saved_size < rows.length) {
+            var message = saved_size.toString() + ' row' + ((saved_size == 1) ? '' : 's') + ' saved! ';
+            message += (rows.length - saved_size).toString() + ' row' + ((rows.length - saved_size == 1) ? ' is' : 's are') + ' incomplete!';
             model.message(message);
             model.status('error');
         }
     }
 
-    var key_to_options = function(key){
+    var key_to_options = function (key) {
         return {
             rows: data['rows'],
-            save_to_url : '/voucher/journal/save/',
-            properties : {id : self.id},
-            onSaveSuccess : function(msg, rows){
+            save_to_url: '/voucher/journal/save/',
+            properties: {id: self.id},
+            onSaveSuccess: function (msg, rows) {
                 validate(msg, rows, key.toDash());
             }
         };
@@ -59,36 +59,57 @@ function JournalVoucher(data){
 
     self.journal_voucher = new TableViewModel(key_to_options('journal_voucher'), JournalVoucherRow);
 
-    self.journal_voucher.cr_total = function(){
+
+    self.journal_voucher.cr_total = function () {
         var total = 0.00;
-        $.each(self.journal_voucher.rows(), function(){
-            if (isAN(this.cr_amount())){
+        $.each(self.journal_voucher.rows(), function () {
+            if (isAN(this.cr_amount())) {
                 total += parseFloat(this.cr_amount());
             }
         });
         return total.toFixed(2);
     }
 
-    self.journal_voucher.dr_total = function(){
+    self.journal_voucher.dr_total = function () {
         var total = 0.00;
-        $.each(self.journal_voucher.rows(), function(){
+        $.each(self.journal_voucher.rows(), function () {
             if (isAN(this.dr_amount()))
                 total += parseFloat(this.dr_amount());
         });
         return total.toFixed(2);
     }
 
-    self.journal_voucher.cr_equals_dr = function(){
+    self.add_row = function (element, viewModel) {
+        $(element).blur();
+        var type;
+        var dr_amount;
+        var cr_amount;
+        var diff = self.journal_voucher.dr_total() - self.journal_voucher.cr_total()
+        if (diff > 0) {
+            type = 'Cr';
+            dr_amount = 0;
+            cr_amount = diff;
+        } else {
+            type = 'Dr';
+            cr_amount = 0;
+            dr_amount = (-1) * diff;
+        }
+
+        if ($(element).closest("tr").is(":nth-last-child(2)") && self.journal_voucher.dr_total() != self.journal_voucher.cr_total())
+            self.journal_voucher.rows.push(new JournalVoucherRow({type: type, cr_amount: cr_amount, dr_amount: dr_amount}));
+    }
+
+    self.journal_voucher.cr_equals_dr = function () {
         return self.journal_voucher.dr_total() === self.journal_voucher.cr_total();
     }
 
-    self.journal_voucher.total_row_class = function(){
+    self.journal_voucher.total_row_class = function () {
         if (self.journal_voucher.dr_total() === self.journal_voucher.cr_total())
             return 'valid-row';
         return 'invalid-row';
     }
 
-    self.journal_voucher.save = function(){
+    self.journal_voucher.save = function () {
 
         self.journal_voucher.status('waiting');
 
@@ -98,82 +119,81 @@ function JournalVoucher(data){
         var selection = $("#journal-voucher > tr");
 
         var error_messages = {
-            1 : 'You can\'t enter both Dr and Cr Accounts in same row!',
-            2 : 'You can\'t enter Cr amount while selecting Dr account!',
-            3 : 'You can\'t enter Dr amount while selecting Cr account!',
-            4 : 'Either Dr or Cr account is required in a row!',
-            5 : 'You can\'t enter both Dr and Cr amounts in same row!',
-            6 : 'Either Dr or Cr amount is required in a row!'
+            1: 'You can\'t enter both Dr and Cr Accounts in same row!',
+            2: 'You can\'t enter Cr amount while selecting Dr account!',
+            3: 'You can\'t enter Dr amount while selecting Cr account!',
+            4: 'Either Dr or Cr account is required in a row!',
+            5: 'You can\'t enter both Dr and Cr amounts in same row!',
+            6: 'Either Dr or Cr amount is required in a row!'
         };
 
         var errors = {}; //row: [errors]
 
-        for (var i=0; i<rows.length; i++){
-
+        for (var i = 0; i < rows.length; i++) {
 
 
             var row_valid = true;
             $(selection[i]).removeClass('invalid-row');
             var row = rows[i];
             errors[i] = [];
-            if (row.dr_account_id() && row.cr_account_id()){
+            if (row.dr_account_id() && row.cr_account_id()) {
 //                message += 'Row ' + (i+1) + ':  ';
                 errors[i].push(1);
                 row_valid = false;
             }
-            if (row.dr_account_id() && row.cr_amount()){
+            if (row.dr_account_id() && row.cr_amount()) {
 //                message += 'Row ' + (i+1) + ':  ';
                 errors[i].push(2);
                 row_valid = false;
             }
-            if (row.cr_account_id() && row.dr_amount()){
+            if (row.cr_account_id() && row.dr_amount()) {
 //                message += 'Row ' + (i+1) + ':  ';
                 errors[i].push(3);
                 row_valid = false;
             }
-            if (!row.dr_account_id() && !row.cr_account_id()){
+            if (!row.dr_account_id() && !row.cr_account_id()) {
 //                message += 'Row ' + (i+1) + ':  ';
                 errors[i].push(4);
                 row_valid = false;
             }
-            if (row.dr_amount() && row.cr_amount()){
+            if (row.dr_amount() && row.cr_amount()) {
 //                message += 'Row ' + (i+1) + ':  ';
                 errors[i].push(5);
                 row_valid = false;
             }
-            if (empty_or_undefined(row.dr_amount()) && empty_or_undefined(row.cr_amount())){
+            if (empty_or_undefined(row.dr_amount()) && empty_or_undefined(row.cr_amount())) {
 //                message += 'Row ' + (i+1) + ':  ';
                 errors[i].push(6);
                 row_valid = false;
             }
-            if (!row_valid){
+            if (!row_valid) {
                 valid = false;
                 $(selection[i]).addClass('invalid-row');
             }
         }
 
-        for ( var r in errors){
+        for (var r in errors) {
             var row_errors = errors[r];
-            message += 'Row ' + (parseInt(r)+1) + ': ';
-            for (var i in row_errors){
+            message += 'Row ' + (parseInt(r) + 1) + ': ';
+            for (var i in row_errors) {
                 message += error_messages[row_errors[i]] + ' ';
             }
             // TODO
 //            message += '<br>';
         }
 
-        if (!self.journal_voucher.cr_equals_dr()){
+        if (!self.journal_voucher.cr_equals_dr()) {
             message += 'Total Dr and Cr amounts don\'t tally!';
             valid = false;
         }
 
-        if (!self.date){
+        if (!self.date) {
             message += 'Date field is required!';
             valid = false;
         }
 
         self.journal_voucher.message(message);
-        if (!valid){
+        if (!valid) {
             self.journal_voucher.status('error');
             return false;
         }
@@ -181,13 +201,13 @@ function JournalVoucher(data){
             type: "POST",
             url: '/voucher/journal/save/',
             data: ko.toJSON(self),
-            success: function(msg){
+            success: function (msg) {
                 self.journal_voucher.message('Saved!');
-                self.deleted_rows  = [];
+                self.deleted_rows = [];
                 self.journal_voucher.status('success');
 
             },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
                 self.journal_voucher.message('Saving Failed!');
                 self.journal_voucher.status('error');
             }
@@ -197,16 +217,32 @@ function JournalVoucher(data){
     }
 }
 
-function JournalVoucherRow(row){
+function JournalVoucherRow(row) {
     var self = this;
 
-    self.type = ko.observable();
+    self.type = ko.observable('Dr');
     self.account = ko.observable();
     self.description = ko.observable();
     self.dr_amount = ko.observable();
     self.cr_amount = ko.observable();
 
-    for (var k in row){
+    self.is_dr = function () {
+        if (self.type() == 'Dr')
+            return true;
+        return false;
+    }
+
+    self.is_cr = function () {
+        if (self.type() == 'Cr')
+            return true;
+        return false;
+    }
+
+    self.type_changed = function (e) {
+//        console.log(self.type());
+    }
+
+    for (var k in row) {
         if (row[k] != null)
             self[k] = ko.observable(row[k]);
     }
