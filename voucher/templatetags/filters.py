9f6c000f04dc55f253072f22cfdfad8a6e79da3e
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-22 18:03:25 +0545

    fieldset template tag

--- voucher/templatetags/filters.py
+++ voucher/templatetags/filters.py
@@ -1,4 +1,6 @@
+import copy
 import json
+from django.utils.datastructures import SortedDict
 from datetime import date, timedelta
 
 from django.core import serializers
@@ -193,3 +195,31 @@ class GroupCheckNode(template.Node):
             return self.nodelist_true.render(context)
         else:
             return self.nodelist_false.render(context)
+
+
+def get_fieldset(parser, token):
+    try:
+        name, fields, as_, variable_name, from_, form = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError('bad arguments for %r' % token.split_contents()[0])
+
+    return FieldSetNode(fields.split(','), variable_name, form)
+
+
+get_fieldset = register.tag(get_fieldset)
+
+
+class FieldSetNode(template.Node):
+    def __init__(self, fields, variable_name, form_variable):
+        self.fields = fields
+        self.variable_name = variable_name
+        self.form_variable = form_variable
+
+    def render(self, context):
+        form = template.Variable(self.form_variable).resolve(context)
+        new_form = copy.copy(form)
+        new_form.fields = SortedDict([(key, value) for key, value in form.fields.items() if key in self.fields])
+
+        context[self.variable_name] = new_form
+
+        return u''
