81f3ed0584ab9e7db5e0a0cfea31d7791b880877
Dipesh Acharya <xtranophilist@gmail.com>
2013-10-25 13:47:23 +0545

    day_between util function

--- js/base.js
+++ js/base.js
@@ -377,3 +377,18 @@ function TableViewModel(options, row_model) {
 
 
 }
+
+function days_between(first, second) {
+
+    // Copy date parts of the timestamps, discarding the time parts.
+    var one = new Date(first.getFullYear(), first.getMonth(), first.getDate());
+    var two = new Date(second.getFullYear(), second.getMonth(), second.getDate());
+
+    // Do the math.
+    var millisecondsPerDay = 1000 * 60 * 60 * 24;
+    var millisBetween = two.getTime() - one.getTime();
+    var days = millisBetween / millisecondsPerDay;
+
+    // Round down.
+    return Math.floor(days);
+}
