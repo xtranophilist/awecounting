015b93d2f52c00451eb195e2d102afbd17b92f22
Dipesh Acharya <xtranophilist@gmail.com>
2013-11-03 16:24:52 +0545

    removed unused sorting algorithm

--- js/base.js
+++ js/base.js
@@ -423,30 +423,3 @@ function hms_to_s(t) { // h:m:s
     var a = t.split(/\D+/);
     return (a[0] * 60 + +a[1]) * 60 + +a[2]
 }
-
-(function () {
-    if (typeof Object.defineProperty === 'function') {
-        try {
-            Object.defineProperty(Array.prototype, 'sortBy', {value: sb});
-        } catch (e) {
-        }
-    }
-    if (!Array.prototype.sortBy) Array.prototype.sortBy = sb;
-
-    function sb(f) {
-        for (var i = this.length; i;) {
-            var o = this[--i];
-            this[i] = [].concat(f.call(o, o, i), o);
-        }
-        this.sort(function (a, b) {
-            for (var i = 0, len = a.length; i < len; ++i) {
-                if (a[i] != b[i]) return a[i] < b[i] ? -1 : 1;
-            }
-            return 0;
-        });
-        for (var i = this.length; i;) {
-            this[--i] = this[i][this[i].length - 1];
-        }
-        return this;
-    }
-})();
