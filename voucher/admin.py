b0a034432a4c75b5580df2808d30fcdbf798455a
Dipesh Acharya <xtranophilist@gmail.com>
2016-01-03 18:28:10 +0545

    Fix cash receipt

--- voucher/admin.py
+++ voucher/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from voucher.models import Invoice, PurchaseVoucher, JournalVoucher
+from voucher.models import Invoice, PurchaseVoucher, JournalVoucher, CashReceipt
 
 admin.site.register(Invoice)
 admin.site.register(PurchaseVoucher)
 admin.site.register(JournalVoucher)
+admin.site.register(CashReceipt)
